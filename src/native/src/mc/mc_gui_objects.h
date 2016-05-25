0abb5e08087a88ae89d12a4a589c841c7a6b1bca
David Dubois <dexcelstraun7@gmail.com>
2016-05-24 22:58:21 -0400

    Comparing buttons is fun!

--- src/native/src/mc/mc_gui_objects.h
+++ src/native/src/mc/mc_gui_objects.h
@@ -6,16 +6,20 @@
 #ifndef RENDERER_MC_GUI_OBJECTS_H
 #define RENDERER_MC_GUI_OBJECTS_H
 
-class mc_gui_button {
+#define MAX_NUM_BUTTONS 22
+
+struct mc_gui_button {
     int x_position;
     int y_position;
     int width;
     int height;
     const char * text;
+    bool is_pressed;
 };
 
 struct mc_gui_screen {
-    mc_gui_button buttons[22];
+    int screen_id;
+    mc_gui_button buttons[MAX_NUM_BUTTONS];
 };
 
 #endif //RENDERER_MC_GUI_OBJECTS_H
