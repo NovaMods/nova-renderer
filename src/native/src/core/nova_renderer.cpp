68277d94c43297aecc048ef4894183c461c50927
David Dubois <dexcelstraun7@gmail.com>
2016-06-18 00:26:38 -0700

    GUI renderer pipeline works

--- src/native/src/core/nova_renderer.cpp
+++ src/native/src/core/nova_renderer.cpp
@@ -14,13 +14,13 @@ INITIALIZE_EASYLOGGINGPP
 
 std::unique_ptr<nova_renderer> nova_renderer::instance;
 
-nova_renderer::nova_renderer() {//: gui_renderer_instance(tex_manager, shader_manager, ubo_manager) {
+nova_renderer::nova_renderer() : gui_renderer_instance(tex_manager, shader_manager, ubo_manager) {
     initialize_logging();
 
     // Oh wow this line is gross. I guess this is why everyone hates CPP?
     game_window = std::unique_ptr<iwindow>(new glfw_gl_window());
 
-    //gui_renderer_instance.build_default_gui();
+    gui_renderer_instance.build_default_gui();
 
     // Here's a bunch of really gross code to Make It Work (TM)
     triangle = std::unique_ptr<ivertex_buffer>(new gl_vertex_buffer());
@@ -59,6 +59,9 @@ void nova_renderer::render_frame() {
     triangle->set_active();
     triangle->draw();
 
+    // Render GUI to GUI buffer
+    gui_renderer_instance.render();
+
     // Render solid geometry
     // Render entities
     // Render transparent things
@@ -75,9 +78,9 @@ void nova_renderer::init_instance() {
     instance = std::unique_ptr<nova_renderer>(new nova_renderer());
 }
 
-//texture_manager & nova_renderer::get_texture_manager() {
-//    return tex_manager;
-//}
+texture_manager & nova_renderer::get_texture_manager() {
+    return tex_manager;
+}
 
 shader_store &nova_renderer::get_shader_manager() {
     return shader_manager;
@@ -87,9 +90,9 @@ uniform_buffer_store &nova_renderer::get_ubo_manager() {
     return ubo_manager;
 }
 
-//gui_renderer & nova_renderer::get_gui_renderer() {
- //   return gui_renderer_instance;
-//}
+gui_renderer & nova_renderer::get_gui_renderer() {
+   return gui_renderer_instance;
+}
 
 
 
