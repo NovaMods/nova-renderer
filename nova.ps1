e69f2d1342f30d77b34314aea3467290c1451a26
David Dubois <dexcelstraun7@gmail.com>
2016-08-17 23:08:30 -0700

    Powershell script now builds all code

--- nova.ps1
+++ nova.ps1
@@ -49,6 +49,117 @@ param (
     [string]$buildEnvironment = "mingw"
 )
 
+################################################################################
+#                                                                              #
+#  Build                                                                       #
+#                                                                              #
+################################################################################
+
+function New-NovaCode([string]$buildEnvironment) {
+    <#
+    .SYNOPSIS
+        Compiles all the Nova code you can possible imagine
+    .PARAMETER buildEnvironment
+        Specifies the build environment you with to use. Must be either 'mingw' or 'msvc'.
+    #>
+
+    Write-Host "Starting compilation of Nova..."
+
+    Set-Location src\native
+
+    if($buildEnvironment -eq "mingw") {
+        Write-Host "Building with MinGW"
+        New-MinGWNovaBuild
+
+    } elseif($buildEnvironment -eq "msvc") {
+        Write-Host "Building wth Visual Studio"
+        New-VisualStudioBuild
+    }
+
+    # I assume that everything worked properly
+
+    # Return us to the previous location
+    Set-Location ..\..
+
+    # Compile the Java code
+
+    gradle build
+
+    Write-Host "Nova compiled!"
+}
+
+function New-MinGWNovaBuild {
+    <#
+    .SYNOPSIS
+        Builds Nova, using MinGW to compile the native code
+    #>
+
+    if(Test-Command -command "mingw32-make.exe") {
+        # MinGW is probably installed, let's assume it is
+        Invoke-CMake -generator "MinGW Makefiles"
+
+        # Compile the code
+        mingw32-make -f Makefile nova-renderer
+
+        # Copy output DLL to the correct location
+        robocopy "." "..\..\run\" "libnova-renderer.dll" 
+
+        Write-Host "Copied Nova, but you're going to have to put libstc++.dll on your path somewhere, or else you won't be able to run Nova."
+
+    } else {
+        Write-Error "Could not call the MinGW Make tool, unable to build Nova. Please install MinGW AND ensure that mingw32-make is in your path"
+    }
+}
+
+function New-VisualStudioBuild {
+    <#
+    .SYNOPSIS
+        Builds the C++ code in Nova, using Visual Studio 2015 to compile the code
+    #>
+
+    if(Test-Command -command "devenv.exe") {
+        # Visual Studio is probably installed, hopefully they have the correct version
+        Invoke-CMake -generator "Visual Studio 14 2015"
+
+        # Compile the code
+        # TODO: Verify that this is the actual name of the solution file
+        devenv NovaRenderer.sln /Build Debug
+
+        # Copy the DLL to the correct location
+        # TODO: Verify that this is the name of the DLL
+        robocopy "." "..\..\run\" "nova-renderer.dll" 
+    } else {
+        Write-Error "Could not call the Visual Studio make tool, unable to build Nova. Please enstall Visual Stusio 2015 and ensure that devenv.exe is on your path"
+    }
+}
+
+function Test-Command([string]$command) {
+    if((Get-Command $command -ErrorAction SilentlyContinue) -eq $null) {
+        Write-Error "Could not find the $command executable. Please ensure that it's in your PATH, then run this script again" -Category ResourceUnavailable
+        return $false
+    } else {
+        # I could return the result of the Get-Command test, but I want to output an error message upon failure
+        return $true
+    }
+}
+
+function Invoke-CMake([string]$generator) {
+    <#
+    .SYNOPSIS
+        Invokes CMake using the provided generator
+    .PARAMETER geneartor 
+        The generator for CMake to use
+    #>
+
+    cmake -G $generator src
+}
+
+################################################################################
+#                                                                              #
+#  Main                                                                        #
+#                                                                              #
+################################################################################
+
 if($setup -eq $true) {
     Initialize-Nova
 }
@@ -63,54 +174,4 @@ if($clean -eq $true) {
 
 if($run -eq $true) {
     Invoke-Nova
-}
-
-function New-NovaCode([string]$buildEnvironment) {
-    <#
-    .SYNOPSIS
-        Compiles all the Nova code you can possible imagine
-    .PARAMETER buildEnvironment
-        Specifies the build environment you with to use. Must be either 'mingw' or 'msvc'.
-    #>
-
-    Write-Host "Starting compilation of Nova..."
-
-    if(Test-Environment -environmentName $buildEnvironment) {
-        # We have the compiler we need! Let's compile some stuff
-        Set-Location src\native
-        Invoke-CMake -
-    }
-}
-
-function Test-Environment([string]$environmentName) {
-    <#
-    .SYNOPSIS
-        Checks if the toolchain for the given build environment is callable from within this script
-    .PARAMETER environmentName
-        The name of the compiler to check. If mingw is the provided value, checks that the MinGW toolchain exists and is set up at least marginally usefully. If the value is msvc, checks that the Visual Studio toolchain exists. Does not check the version of Visual Studio installed because that will be checked when we try to build Nova and I don't want to do it here
-    #>
-
-    if($environmentName -eq "mingw") {
-        # We can simply test for mingw32-make. As long as we have that, everything else might work
-        if((Get-Command "mingw32-make.exe" -ErrorAction SilentlyContinue) -eq $null) {
-            Write-Error "Could not find the mingw32-make.exe executable. Please ensure that it's in your PATH, then run this script again" -Category ResourceUnavailable
-            return $false
-        } else {
-            # I could return the result of the Get-Command test, but I want to output an error message upon failure
-            return $true
-        }
-
-    } elseif($environmentName -eq "msvc") {
-        # Check for the msvc build thingy
-        if((Get-Command "devenv.exe" -ErrorAction SilentlyContinue) -eq $null) {
-            Write-Error "Could not find the devenv.exe executable. Please ensure that it's in your PATH, then run this script again" -Category ResourceUnavailable
-            return $false
-        } else {
-            # Again, I could just output the result of the Get-Command test, but I definitely want to output error messages whenever possible 
-            return $true
-        }
-    } else {
-        Write-Error "Compiler thing $environmentName not supported. Please specify either mingw or msvc" -Category InvalidArgument
-        return $false
-    }
 }
