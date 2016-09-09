8fee20a8176213872c843b764543865551b2ef35
David Dubois <dexcelstraun7@gmail.com>
2016-09-09 08:47:21 -0700

    UPdated ps1, texture code in place

--- src/main/cpp/model/data_model.h
+++ src/main/cpp/model/data_model.h
@@ -34,9 +34,13 @@ namespace nova {
 
         private:
             std::unordered_map<std::string, gl_shader_program> loaded_shaderpack;
+            std::unordered_map<std::string, GLuint> loaded_textures;
             std::string loaded_shaderpack_name;
+            std::vector<std::string> loaded_resourcepack_names;
 
             void load_new_shaderpack(const std::string &new_shaderpack_name) noexcept;
+
+            void load_new_resourcepack(const std::vector<std::string>&resourcepack_names);
         };
     }
 }
