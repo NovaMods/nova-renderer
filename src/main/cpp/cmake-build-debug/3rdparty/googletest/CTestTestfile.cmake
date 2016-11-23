fdf7afa8c5677bf00f77032ed5666a8a8db3ec01
David Dubois <dexcelstraun7@gmail.com>
2016-11-22 23:40:57 -0800

    Removing CLion's new build files -_-

--- src/main/cpp/cmake-build-debug/3rdparty/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Documents/Nova Renderer/src/main/cpp/3rdparty/googletest
-# Build directory: D:/Documents/Nova Renderer/src/main/cpp/cmake-build-debug/3rdparty/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(googlemock)
--- src/main/cpp/cmake-build-debug/3rdparty/json/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Documents/Nova Renderer/src/main/cpp/3rdparty/json
-# Build directory: D:/Documents/Nova Renderer/src/main/cpp/cmake-build-debug/3rdparty/json
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(test)
