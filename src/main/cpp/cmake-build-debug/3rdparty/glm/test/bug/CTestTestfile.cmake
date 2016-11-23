28ca1264faf5db1843f07b0000d541b816350d17
David Dubois <dexcelstraun7@gmail.com>
2016-11-22 23:40:57 -0800

    Removing CLion's new build files -_-

--- src/main/cpp/cmake-build-debug/3rdparty/glm/test/bug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Documents/Nova Renderer/src/main/cpp/3rdparty/glm/test/bug
-# Build directory: D:/Documents/Nova Renderer/src/main/cpp/cmake-build-debug/3rdparty/glm/test/bug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- src/main/cpp/cmake-build-debug/3rdparty/glm/test/gtc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Documents/Nova Renderer/src/main/cpp/3rdparty/glm/test/gtc
-# Build directory: D:/Documents/Nova Renderer/src/main/cpp/cmake-build-debug/3rdparty/glm/test/gtc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
