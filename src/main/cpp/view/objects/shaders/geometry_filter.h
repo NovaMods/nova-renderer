d18087b758eb0b94e34ce9f3cf4fff9aa06e1915
David Dubois <dexceltraun7@gmail.com>
2016-12-30 13:24:05 -0500

    Moving geometry_filter to its own file

---
+++ src/main/cpp/view/objects/shaders/geometry_filter.h
@@ -0,0 +1,84 @@
+#ifndef GEOMETRY_FILTER_H
+#define GEOMETRY_FILTER_H
+
+#include <vector>
+#include <string>
+#include <optional.hpp>
+
+#include "view/objects/render_object.h"
+
+namespace nova {
+    namespace view {
+        /*!
+         * \brief Holds a bunch of information to filter geometry
+         *
+         * At first I used lambdas. Lambdas are very powerful: they can do anything. Anything!
+         *
+         * The lambdas were super hard to debug, though. I stored references to them in some kinda of structure, and
+         * those references were really bad at pointing back to the line of code where the lambda was actually
+         * constructed. I couldn't see what was actually happening. this would be fine, except that it wasn't and I
+         * kept getting crashes that were super hard to debug.
+         *
+         * Now there's a simple data structure. Data is much easier to debug. No more stupid crashes for me!
+         *
+         * (i hope)
+         *
+         * There's a precidence for these filters:
+         *  - If one of the optional values isn't set, then it's ignored.
+         *  - Block, Entity, Sky, Particle, Geometry Type, and Name filters are applied first. They're all OR'd together
+         *  - Transparent, Cutout, Damaged, and Emissive are applied next, OR'd together
+         *
+         * This means you can have a filter which matches transparent blocks and entities, or all cutout blocks of a
+         * certain geometry type, but you can't have a filter that matches cutout blocks and emissive particles. If you
+         * need that, too bad.
+         */
+        struct geometry_filter {
+            // Using a third-party library because CMake doesn't (yet) support C++17 (probably, updating tool is hard)
+            std::experimental::optional<bool> should_be_block;
+            std::experimental::optional<bool> should_be_selection_box;
+            std::experimental::optional<bool> should_be_entity;
+            std::experimental::optional<bool> should_be_particle;
+            std::experimental::optional<bool> should_be_sky_object;
+
+            std::vector<geometry_type> geometry_types;
+
+            std::vector<std::string>> names;
+            std::vector<std::string>> name_parts;
+
+            std::experimental::optional<bool> should_be_transparent;
+            std::experimental::optional<bool> should_be_cutout;
+            std::experimental::optional<bool> should_be_emissive;
+            std::experimental::optional<bool> should_be_damaged;
+        };
+
+        /*
+         * A bunch of functions to operate on a geometry filter
+         *
+         * These are separate functions and not methods because this way, it's easier to map them to the values stored 
+         * in the shaders.json file
+         */
+
+        void accept_block(geometry_filter& filter);
+
+        void reject_block(geometry_filter& filter);
+
+        void accept_entity(geometry_filter& filter);
+
+        void reject_entity(geometry_filter& filter);
+
+        void accept_selection_box(geometry_filter& filter);
+
+        void reject_selection_box(geometry_filter& filter);
+
+        void accept_particle(geometry_filter& filter);
+
+        void reject_particle(geometry_filter& filter);
+
+        void accept_sky_object(geometry_filter& filter);
+        void reject_sky_object(geometry_filter& filter);
+
+        void accept_geometry_type(geometry_filter& filter, geometry_type type);
+    }
+}
+
+#endif
