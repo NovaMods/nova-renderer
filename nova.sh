b530d5848f6fe560321e16d027620dc628f25c0a
Tobias Schulz <tobias.schulz186@gmail.com>
2017-01-29 20:35:04 +0100

    fix linux

--- nova.sh
+++ nova.sh
@@ -40,14 +40,14 @@ setup_nova() {
         exit -1
     fi
 
-    cp -r src/minecraft src/main/java
+    cp -r src/minecraft/* src/main/java
     echo "Unpacked MCP"
 
     rm -rf src/minecraft
     rm -rf mcp
 
     echo "Injecting MCP into Minecraft..."
-    patch -p0 -i src/main/resources/patches/nova.diff
+    patch -p1 -i src/main/resources/patches/nova.patch
     if [ $? != "0" ]; then
         echo "Failed to apply patches. Tell DethRaid about it and give him the full console log"
         exit -2
@@ -59,12 +59,16 @@ setup_nova() {
     echo "Nova set up successfully"
 }
 
+compile_java() {
+    gradle fatjar
+}
+
 compile_native_code() {
     echo "Compiling native code"
     mkdir -p target/cpp
     cd target/cpp
 
-    cmake -g "Unix Makefiles" ../../src/main/cpp 
+    cmake -g "Unix Makefiles" ../../src/main/cpp
     if [ $? != "0" ]; then
         echo "Failed to execute CMake. Possibly you're missing the Vulkan library, which GLFW requires because it's stupid"
         exit -3
@@ -78,17 +82,27 @@ compile_native_code() {
 }
 
 compile_all() {
+    compile_java
     compile_native_code
 }
 
-case $1 in 
-"setup") 
+start_nova() {
+    cd jars
+    export LD_LIBRARY_PATH=../target/cpp
+    java -classpath ../build/classes/main:libraries:libraries/com/google/code/findbugs/jsr305/3.0.1/jsr305-3.0.1.jar:libraries/com/google/code/gson/gson/2.2.4/gson-2.2.4.jar:libraries/com/google/guava/guava/17.0/guava-17.0.jar:libraries/com/ibm/icu/icu4j-core-mojang/51.2/icu4j-core-mojang-51.2.jar:libraries/com/mojang/authlib/1.5.22/authlib-1.5.22.jar:libraries/com/mojang/netty/1.6/netty-1.6.jar:libraries/com/mojang/realms/1.9.1/realms-1.9.1.jar:libraries/com/paulscode/codecjorbis/20101023/codecjorbis-20101023.jar:libraries/com/paulscode/codecwav/20101023/codecwav-20101023.jar:libraries/com/paulscode/libraryjavasound/20101123/libraryjavasound-20101123.jar:libraries/com/paulscode/librarylwjglopenal/20100824/librarylwjglopenal-20100824.jar:libraries/com/paulscode/soundsystem/20120107/soundsystem-20120107.jar:libraries/commons-codec/commons-codec/1.9/commons-codec-1.9.jar:libraries/commons-io/commons-io/2.4/commons-io-2.4.jar:libraries/commons-logging/commons-logging/1.1.3/commons-logging-1.1.3.jar:libraries/io/netty/netty-all/4.0.23.Final/netty-all-4.0.23.Final.jar:libraries/it/unimi/dsi/fastutil/7.0.12_mojang/fastutil-7.0.12_mojang.jar:libraries/net/java/dev/jna/jna/3.4.0/jna-3.4.0.jar:libraries/net/java/dev/jna/platform/3.4.0/platform-3.4.0.jar:libraries/net/java/jinput/jinput/2.0.5/jinput-2.0.5.jar:libraries/net/java/jinput/jinput-platform/2.0.5/jinput-platform-2.0.5-natives-windows.jar:libraries/net/java/jutils/jutils/1.0.0/jutils-1.0.0.jar:libraries/net/sf/jopt-simple/jopt-simple/4.6/jopt-simple-4.6.jar:libraries/org/apache/commons/commons-compress/1.8.1/commons-compress-1.8.1.jar:libraries/org/apache/commons/commons-lang3/3.3.2/commons-lang3-3.3.2.jar:libraries/org/apache/httpcomponents/httpclient/4.3.3/httpclient-4.3.3.jar:libraries/org/apache/httpcomponents/httpcore/4.3.2/httpcore-4.3.2.jar:libraries/org/apache/logging/log4j/log4j-api/2.0-beta9/log4j-api-2.0-beta9.jar:libraries/org/apache/logging/log4j/log4j-core/2.0-beta9/log4j-core-2.0-beta9.jar:libraries/org/lwjgl/lwjgl/lwjgl/2.9.4-nightly-20150209/lwjgl-2.9.4-nightly-20150209.jar:libraries/org/lwjgl/lwjgl/lwjgl-platform/2.9.4-nightly-20150209/lwjgl-platform-2.9.4-nightly-20150209-natives-windows.jar:libraries/org/lwjgl/lwjgl/lwjgl_util/2.9.4-nightly-20150209/lwjgl_util-2.9.4-nightly-20150209.jar:libraries/oshi-project/oshi-core/1.1/oshi-core-1.1.jar:versions/1.10/1.10.jar -Djava.library.path=versions/1.10/1.10-natives Start
+}
+
+case $1 in
+"setup")
     setup_nova
     ;;
 "build")
     compile_all
     ;;
-*) 
+"start")
+    start_nova
+    ;;
+*)
     show_help
     ;;
 esac
