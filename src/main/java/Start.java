1d6150c908b41cc96124e3e9a4ce17740e635b57
David Dubois <dexcelstraun7@gmail.com>
2017-02-09 09:44:21 -0500

    Gonna load in chunk definitions

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,12 +1,14 @@
-import net.minecraft.client.main.Main;
-
+import java.io.File;
+import java.lang.reflect.Field;
 import java.util.Arrays;
+
+import net.minecraft.client.main.Main;
 
 public class Start
 {
     public static void main(String[] args)
     {
-        Main.main(concat(new String[] {"--version", "mcp", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.10", "--userProperties", "{}"}, args));
+        Main.main(concat(new String[] {"--version", "mcp", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties", "{}"}, args));
     }
 
     public static <T> T[] concat(T[] first, T[] second)
---
+++ src/main/java/com/continuum/nova/chunks/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains all the code needed to process and render chunks
+ * <p>There's a good chance this won't have a lot. All well</p>
+ *
+ * @author ddubois
+ */
+package com.continuum.nova.chunks;
