827a6bb6babdea6113726ed611b9a541f2abc836
Tobias Schulz <tobias.schulz@linux.com>
2017-06-22 01:56:38 +0200

    dont put context lines in the patch file

--- nova.ps1
+++ nova.ps1
@@ -271,7 +271,7 @@ function Invoke-Nova {
 function New-Patches {
     Set-Location src\main\java\net
     $p = "..\..\..\..\patches\nova.patch"
-    git diff origin/minecraft-1.10-mcp > $p
+    git diff -U0 origin/minecraft-1.10-mcp > $p
     $content = Get-Content $p
     $encodingUtf8WithoutByteOrderMark = New-Object System.Text.UTF8Encoding($False)
     [System.IO.File]::WriteAllLines($p, $content, $encodingUtf8WithoutByteOrderMark)
