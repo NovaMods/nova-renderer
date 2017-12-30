93766eae62ae82ba593d20724e33dcb94bea57cf
David Dubois <dexcelstraun7@gmail.com>
2017-12-30 18:34:14 -0500

    Revert "Converting to jsoncpp"
    This reverts commit d06d780555521d11127cb53fb1dce8354623ce79.

---
+++ src/main/cpp/utils/value_or_err.h
@@ -0,0 +1,17 @@
+//
+// Created by ddubois on 12/28/17.
+//
+
+#ifndef RENDERER_VALUE_OR_ERR_H
+#define RENDERER_VALUE_OR_ERR_H
+
+namespace nova {
+    /*!
+     *
+     */
+    class value_or_err {
+
+    };
+}
+
+#endif //RENDERER_VALUE_OR_ERR_H
