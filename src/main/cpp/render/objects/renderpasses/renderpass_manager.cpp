b1227f7d3a902a35f9479ac2cbcd5dbfbbb47a3c
David Dubois <dexcelstraun7@gmail.com>
2018-03-31 11:29:24 -0700

    Created renderpasses and framebuffers

--- src/main/cpp/render/objects/renderpasses/renderpass_manager.cpp
+++ src/main/cpp/render/objects/renderpasses/renderpass_manager.cpp
@@ -12,12 +12,16 @@
 namespace nova {
     vk::RenderPass make_render_pass(const render_pass& pass, std::shared_ptr<texture_manager> textures, std::shared_ptr<render_context> context);
 
-    std::unordered_map<std::string, vk::RenderPass> make_passes(const shaderpack_data& data, std::shared_ptr<texture_manager> textures,
-                                                                std::shared_ptr<render_context> context) {
-        std::unordered_map<std::string, vk::RenderPass> renderpasses;
+    vk::Framebuffer make_framebuffer(const render_pass &pass, const vk::RenderPass renderpass, std::shared_ptr<texture_manager> textures, std::shared_ptr<render_context> context);
+
+    std::unordered_map<std::string, std::pair<vk::RenderPass, vk::Framebuffer>> make_passes(const shaderpack_data& data, std::shared_ptr<texture_manager> textures,
+                                                                                            std::shared_ptr<render_context> context) {
+        std::unordered_map<std::string, std::pair<vk::RenderPass, vk::Framebuffer>> renderpasses;
 
         for(const auto& named_pass : data.passes) {
-            renderpasses[named_pass.first] = make_render_pass(named_pass.second, textures, context);
+            const auto renderpass = make_render_pass(named_pass.second, textures, context);
+            renderpasses[named_pass.first].first = renderpass;
+            renderpasses[named_pass.first].second = make_framebuffer(named_pass.second, renderpass, textures, context);
         }
 
         return renderpasses;
@@ -28,17 +32,36 @@ namespace nova {
         std::vector<vk::AttachmentDescription> attachments;
 
         std::vector<vk::AttachmentReference> color_refs;
+        optional<vk::AttachmentReference> depth_ref;
 
         if(pass.texture_outputs) {
             auto num_supported_attachments = context->gpu.props.limits.maxColorAttachments;
             const auto& texture_outputs_vec = pass.texture_outputs.value();
+
+            // The size and name of the attachment at index n - 1, allowing us to validate that all the textures in a
+            // given pass are the same size
+            vk::Extent2D last_texture_size;
+            std::string last_texture_name;
+
             if(texture_outputs_vec.size() > num_supported_attachments) {
                 LOG(ERROR) << "You're trying to use " << texture_outputs_vec.size() << " color attachments with pass "
                            << pass.name << ", but your GPU only supports " << num_supported_attachments;
 
             } else {
+                const auto& first_tex = textures->get_texture(texture_outputs_vec[0]);
+                last_texture_size = first_tex.get_size();
+                last_texture_name = first_tex.get_name();
+
                 for(const auto &color_attachment_name : texture_outputs_vec) {
                     const auto &texture = textures->get_texture(color_attachment_name);
+
+                    if(texture.get_size() != last_texture_size) {
+                        LOG(ERROR) << "Texture " << texture.get_name() << " does not have the same size as texture "
+                                   << last_texture_name << ". In order to use two textures in the same pass, they must have the same size";
+                    }
+
+                    last_texture_size = texture.get_size();
+                    last_texture_name = texture.get_name();
 
                     vk::AttachmentDescription color_attachment = {};
                     color_attachment.format = texture.get_format();
@@ -46,23 +69,45 @@ namespace nova {
                     color_attachment.loadOp = vk::AttachmentLoadOp::eLoad;
                     color_attachment.initialLayout = vk::ImageLayout::eColorAttachmentOptimal;
                     color_attachment.finalLayout = vk::ImageLayout::eColorAttachmentOptimal;
+                    color_attachment.storeOp = vk::AttachmentStoreOp::eStore;
 
+                    auto color_ref = vk::AttachmentReference()
+                        .setAttachment(static_cast<uint32_t>(attachments.size()))
+                        .setLayout(vk::ImageLayout::eColorAttachmentOptimal);
+
+                    color_refs.push_back(color_ref);
                     attachments.push_back(color_attachment);
                 }
             }
         }
 
-        if(pass.texture_inputs) {
-            vk::AttachmentReference ref = {};
-            ref.attachment = static_cast<uint32_t>(attachments.size() - 1);
-            ref.layout = vk::ImageLayout::eColorAttachmentOptimal;
-            color_refs.push_back(ref);
+        if(pass.depth_output) {
+            const auto& depth_tex = textures->get_texture(pass.depth_output.value());
+
+            auto depth_attachment = vk::AttachmentDescription()
+                .setFormat(depth_tex.get_format())
+                .setSamples(vk::SampleCountFlagBits::e1)
+                .setLoadOp(vk::AttachmentLoadOp::eLoad)
+                .setInitialLayout(vk::ImageLayout::eDepthStencilAttachmentOptimal)
+                .setFinalLayout(vk::ImageLayout::eDepthStencilAttachmentOptimal)
+                .setStoreOp(vk::AttachmentStoreOp::eStore)
+                .setStencilLoadOp(vk::AttachmentLoadOp::eLoad)
+                .setStencilStoreOp(vk::AttachmentStoreOp::eStore);
+
+            depth_ref = vk::AttachmentReference{};
+            depth_ref->setAttachment(static_cast<uint32_t>(attachments.size()));
+            depth_ref->setLayout(vk::ImageLayout::eDepthStencilAttachmentOptimal);
+
+            attachments.push_back(depth_attachment);
         }
 
         vk::SubpassDescription subpass = {};
         subpass.pipelineBindPoint = vk::PipelineBindPoint::eGraphics;
         subpass.colorAttachmentCount = static_cast<uint32_t>(color_refs.size());
         subpass.pColorAttachments = color_refs.data();
+        if(depth_ref) {
+            subpass.setPDepthStencilAttachment(&depth_ref.value());
+        }
 
         vk::RenderPassCreateInfo render_pass_create_info = {};
         render_pass_create_info.attachmentCount = static_cast<uint32_t>(attachments.size());
@@ -71,105 +116,44 @@ namespace nova {
         render_pass_create_info.pSubpasses = &subpass;
         render_pass_create_info.dependencyCount = 0;
 
-        final_pass = context->device.createRenderPass(render_pass_create_info);
+        return context->device.createRenderPass(render_pass_create_info);
     }
 
 
-    void renderpass_manager::create_final_renderpass(const vk::Extent2D& window_size) {
-        std::vector<vk::AttachmentDescription> attachments;
+    vk::Framebuffer make_framebuffer(const render_pass &pass, const vk::RenderPass renderpass, std::shared_ptr<texture_manager> textures,
+                                     std::shared_ptr<render_context> context) {
+        std::vector<vk::ImageView> attachments;
+        vk::Extent2D framebuffer_size;
+        if(pass.texture_outputs) {
+            const auto& outputs = pass.texture_outputs.value();
 
-        std::vector<vk::AttachmentReference> color_refs;
-
-        // Just one color buffer in the final pass
-        vk::AttachmentDescription color_attachment = {};
-        color_attachment.format = context->swapchain_format;
-        color_attachment.samples = vk::SampleCountFlagBits::e1;
-        color_attachment.loadOp = vk::AttachmentLoadOp::eDontCare;
-        color_attachment.initialLayout = vk::ImageLayout::eUndefined;
-        color_attachment.finalLayout = vk::ImageLayout::ePresentSrcKHR;
-
-        attachments.push_back(color_attachment);
-
-        vk::AttachmentReference ref = {};
-        ref.attachment = static_cast<uint32_t>(attachments.size() - 1);
-        ref.layout = vk::ImageLayout::eColorAttachmentOptimal;
-        color_refs.push_back(ref);
-
-        vk::SubpassDescription subpass = {};
-        subpass.pipelineBindPoint = vk::PipelineBindPoint::eGraphics;
-        subpass.colorAttachmentCount = static_cast<uint32_t>(color_refs.size());
-        subpass.pColorAttachments = color_refs.data();
-
-        vk::RenderPassCreateInfo render_pass_create_info = {};
-        render_pass_create_info.attachmentCount = static_cast<uint32_t>(attachments.size());
-        render_pass_create_info.pAttachments = attachments.data();
-        render_pass_create_info.subpassCount = 1;
-        render_pass_create_info.pSubpasses = &subpass;
-        render_pass_create_info.dependencyCount = 0;
-
-        final_pass = context->device.createRenderPass(render_pass_create_info);
-
-        LOG(TRACE) << "Created final renderpass";
-    }
-
-    void renderpass_manager::create_main_renderpass(const vk::Extent2D& window_size) {
-        /*main_pass = renderpass_builder()
-                .set_framebuffer_size(window_size.width, window_size.height)
-                .add_color_buffer()
-                .build();
-
-        LOG(TRACE) << "Created main renderpass";*/
-    }
-
-    const vk::RenderPass renderpass_manager::get_main_renderpass() const {
-        return main_pass;
-    }
-
-    const vk::RenderPass renderpass_manager::get_final_renderpass() const {
-        return final_pass;
-    }
-
-    void renderpass_manager::rebuild_all(const vk::Extent2D& main_shadow_size, const vk::Extent2D& light_shadow_size, const vk::Extent2D& window_size) {
-        create_main_renderpass(window_size);
-        create_final_renderpass(window_size);
-
-        create_final_framebuffers(window_size);
-    }
-
-    void renderpass_manager::create_final_framebuffers(const vk::Extent2D &window_size) {
-        final_framebuffers.reserve(context->swapchain_image_views.size());
-
-        for(size_t i = 0; i < context->swapchain_image_views.size(); i++) {
-            vk::ImageView attachments[] = {
-                    context->swapchain_image_views[i]
-            };
-
-            vk::FramebufferCreateInfo framebuffer_create_info = {};
-            framebuffer_create_info.renderPass = final_pass;
-            framebuffer_create_info.attachmentCount = 1;
-            framebuffer_create_info.pAttachments = attachments;
-            framebuffer_create_info.width = window_size.width;
-            framebuffer_create_info.height = window_size.height;
-            framebuffer_create_info.layers = 1;
-
-            final_framebuffers.push_back(context->device.createFramebuffer(framebuffer_create_info));
+            for(const auto& output_name : outputs) {
+                const auto& tex = textures->get_texture(output_name);
+                attachments.push_back(tex.get_image_view());
+                framebuffer_size = tex.get_size();
+            }
         }
-    }
-
-    const vk::Framebuffer renderpass_manager::get_framebuffer(uint32_t framebuffer_idx) const {
-        return final_framebuffers[framebuffer_idx];
-    }
-
-    renderpass_manager::~renderpass_manager() {
-        auto device = context->device;
-
-        device.destroyRenderPass(main_shadow_pass);
-        device.destroyRenderPass(light_shadow_pass);
-        device.destroyRenderPass(main_pass);
-        device.destroyRenderPass(final_pass);
-
-        for(auto& fb : final_framebuffers) {
-            device.destroyFramebuffer(fb);
+        if(pass.depth_output) {
+            const auto& depth_tex_name = pass.depth_output.value();
+            const auto& depth_tex = textures->get_texture(depth_tex_name);
+            attachments.push_back(depth_tex.get_image_view());
+            framebuffer_size = depth_tex.get_size();
         }
+
+        if(!attachments.empty()) {
+            auto framebuffer_create_info = vk::FramebufferCreateInfo()
+                    .setAttachmentCount(attachments.size())
+                    .setPAttachments(attachments.data())
+                    .setRenderPass(renderpass)
+                    .setWidth(framebuffer_size.width)
+                    .setHeight(framebuffer_size.height)
+                    .setLayers(1);
+
+            return context->device.createFramebuffer(framebuffer_create_info);
+        }
+
+        LOG(ERROR) << "No framebuffer attachments for pass " << pass.name << ". This is an error and you should fix it";
+
+        return vk::Framebuffer();
     }
 }
--- src/main/cpp/render/objects/renderpasses/renderpass_manager.h
+++ src/main/cpp/render/objects/renderpasses/renderpass_manager.h
@@ -12,63 +12,9 @@
 #include "../textures/texture_manager.h"
 
 namespace nova {
-    std::unordered_map<std::string, vk::RenderPass> make_passes(const shaderpack_data& data, std::shared_ptr<texture_manager> textures,
-                                                                std::shared_ptr<render_context> context);
-
-    /*!
-     * \brief Holds all the render passes that we made from the loaded shaderpack
-     *
-     * Nova explicitly defines a few renderpasses:
-     *  - Virtual texture pass to see what textures are needed
-     *  - shadows
-     *  - Gbuffers and lighting
-     *      - Two options for the pass:
-     *          - if the shaderpack uses deferres passes, opaques in one subpass, then block lights, then deferred
-     *          passes, then transparents
-     *          - If the shaderpack does not use deferred passes, opaques in one subpass, then transparents, then block l
-     *          ights
-     *      - Might want to use subpasses for the composites as well
-     *  - Final
-     *
-     * each of those is split into subpases based on render type (fully opaque, cutout, transparent, etc)
-     */
-    class renderpass_manager {
-    public:
-        renderpass_manager(const shaderpack_data& data, std::shared_ptr<texture_manager> textures, std::shared_ptr<render_context> context);
-
-        ~renderpass_manager();
-
-        /*!
-         * \brief Rebuilds the entire renderpasses
-         *
-         * Renderpasses are dependent on the framebuffer size so there we go
-         *
-         * And since there's new renderpasses we'll need to rebuild our pipelines as well...
-         *
-         * \param window_size The size of the window we're rendering to
-         */
-        void rebuild_all(const vk::Extent2D& main_shadow_size, const vk::Extent2D& light_shadow_size, const vk::Extent2D& window_size);
-
-        const vk::RenderPass get_main_renderpass() const;
-        const vk::RenderPass get_final_renderpass() const;
-        const vk::Framebuffer get_framebuffer(uint32_t framebuffer_idx) const;
-
-    private:
-        vk::RenderPass main_shadow_pass;
-        vk::RenderPass light_shadow_pass;
-        vk::RenderPass main_pass;
-        vk::RenderPass final_pass;
-
-        std::vector<vk::Framebuffer> final_framebuffers;
-
-        void create_final_renderpass(const vk::Extent2D& window_size);
-
-        void create_main_renderpass(const vk::Extent2D& window_size);
-
-        void create_final_framebuffers(const vk::Extent2D &window_size);
-
-        std::shared_ptr<render_context> context;
-    };
+    std::unordered_map<std::string, std::pair<vk::RenderPass, vk::Framebuffer>> make_passes(const shaderpack_data& data,
+                                                                                            std::shared_ptr<texture_manager> textures,
+                                                                                            std::shared_ptr<render_context> context);
 }
 
 #endif //RENDERER_RENDER_PASS_MANAGER_H
