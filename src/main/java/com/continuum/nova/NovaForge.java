9903b02c7ee14f1b8d4e6f7ce93501ff253661d0
Jannis Lübke <werbung.janrupf@t-online.de>
2018-06-12 22:02:07 +0200

    Fixed and improved autogenerated code

--- src/main/java/com/continuum/nova/NovaForge.java
+++ src/main/java/com/continuum/nova/NovaForge.java
@@ -1,14 +1,9 @@
 package com.continuum.nova;
 
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.event.RegistryEvent;
 
 @Mod(
         modid = NovaForge.MOD_ID,
@@ -17,8 +12,8 @@ import net.minecraftforge.event.RegistryEvent;
 )
 public class NovaForge {
 
-    public static final String MOD_ID = "nova";
-    public static final String MOD_NAME = "Nova";
+    public static final String MOD_ID = "nova-renderer";
+    public static final String MOD_NAME = "Nova renderer";
     public static final String VERSION = "0.0.1-SNAPSHOT";
 
     /** This is the instance of your mod as created by Forge. It will never be null. */
@@ -49,58 +44,4 @@ public class NovaForge {
     public void postinit(FMLPostInitializationEvent event) {
 
     }
-
-    /**
-     * Forge will automatically look up and bind blocks to the fields in this class
-     * based on their registry name.
-     */
-    @GameRegistry.ObjectHolder(MOD_ID)
-    public static class Blocks {
-      /*
-          public static final MySpecialBlock mySpecialBlock = null; // placeholder for special block below
-      */
-    }
-
-    /**
-     * Forge will automatically look up and bind items to the fields in this class
-     * based on their registry name.
-     */
-    @GameRegistry.ObjectHolder(MOD_ID)
-    public static class Items {
-      /*
-          public static final ItemBlock mySpecialBlock = null; // itemblock for the block above
-          public static final MySpecialItem mySpecialItem = null; // placeholder for special item below
-      */
-    }
-
-    /**
-     * This is a special class that listens to registry events, to allow creation of mod blocks and items at the proper time.
-     */
-    @Mod.EventBusSubscriber
-    public static class ObjectRegistryHandler {
-       /** Listen for the register event for creating custom items */
-       @SubscribeEvent
-       public static void addItems(RegistryEvent.Register<Item> event) {
-           /*
-             event.getRegistry().register(new ItemBlock(Blocks.myBlock).setRegistryName(MOD_ID, "myBlock"));
-             event.getRegistry().register(new MySpecialItem().setRegistryName(MOD_ID, "mySpecialItem"));
-            */
-       }
-       /** Listen for the register event for creating custom blocks */
-       @SubscribeEvent
-       public static void addBlocks(RegistryEvent.Register<Block> event) {
-           /*
-             event.getRegistry().register(new MySpecialBlock().setRegistryName(MOD_ID, "mySpecialBlock"));
-            */
-       }
-    }
-    /* EXAMPLE ITEM AND BLOCK - you probably want these in separate files
-    public static class MySpecialItem extends Item {
-
-    }
-
-    public static class MySpecialBlock extends Block {
-
-    }
-    */
 }
