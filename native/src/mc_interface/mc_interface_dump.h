97b151096ffa23fdda9b1ce7ae71e1595c948e2a
David Dubois <dexcelstraun7@gmail.com>
2018-08-04 10:05:24 -0700

    Less error-prone disabling to debug dump

--- native/src/mc_interface/mc_interface_dump.h
+++ native/src/mc_interface/mc_interface_dump.h
@@ -16,18 +16,30 @@
 #define W_FLOAT(x) do { float d[] = { (float) x }; dump_out.write((char*) d, sizeof(float)); } while(false)
 #define W_DOUBLE(x) do { double d[] = { (double) x }; dump_out.write((char*) d, sizeof(double)); } while(false)
 
+#else
+
+#define DUMP_FUNC
+#define W_UINT32(x)
+#define W_FLOAT(x)
+#define W_DOUBLE(x)
+
+#endif
+
 using namespace nova;
 
 static std::ofstream dump_out;
 static std::mutex dump_lock;
 void dump_initialize() {
+#ifdef DUMP_NOVA_CALLS
     dump_out.open("nova_debug_dump", std::ios::out | std::ios::trunc | std::ios::binary);
     dump_lock.lock();
     DUMP_FUNC;
     dump_lock.unlock();
+#endif
 }
 
 void dump_add_texture(mc_atlas_texture* texture) {
+#ifdef DUMP_NOVA_CALLS
     dump_lock.lock();
     DUMP_FUNC;
     W_UINT32(texture->width);
@@ -37,16 +49,20 @@ void dump_add_texture(mc_atlas_texture* texture) {
     dump_out.write(texture->name, strlen(texture->name) + 1);
     dump_out.flush();
     dump_lock.unlock();
+#endif
 }
 
 void dump_reset_texture_manager() {
+#ifdef DUMP_NOVA_CALLS
     dump_lock.lock();
     DUMP_FUNC;
     dump_out.flush();
     dump_lock.unlock();
+#endif
 }
 
 void dump_send_lightmap_texture(uint32_t* data, int count, int width, int height) {
+#ifdef DUMP_NOVA_CALLS
     dump_lock.lock();
     DUMP_FUNC;
     W_UINT32(count);
@@ -55,9 +71,11 @@ void dump_send_lightmap_texture(uint32_t* data, int count, int width, int height
     dump_out.write((char*) data, width * height * sizeof(uint32_t));
     dump_out.flush();
     dump_lock.unlock();
+#endif
 }
 
 void dump_add_texture_location(mc_texture_atlas_location* location) {
+#ifdef DUMP_NOVA_CALLS
     dump_lock.lock();
     DUMP_FUNC;
     dump_out.write((char*) location->name, strlen(location->name) + 1);
@@ -67,15 +85,19 @@ void dump_add_texture_location(mc_texture_atlas_location* location) {
     W_FLOAT(location->max_v);
     dump_out.flush();
     dump_lock.unlock();
+#endif
 }
 
 void dump_get_max_texture_size() {
+#ifdef DUMP_NOVA_CALLS
     dump_lock.lock();
     DUMP_FUNC;
     dump_lock.unlock();
+#endif
 }
 
 void dump_add_chunk_geometry_for_filter(const char* filter_name, mc_chunk_render_object * chunk) {
+#ifdef DUMP_NOVA_CALLS
     dump_lock.lock();
     DUMP_FUNC;
     dump_out.write((char*) filter_name, strlen(filter_name) + 1);
@@ -90,9 +112,11 @@ void dump_add_chunk_geometry_for_filter(const char* filter_name, mc_chunk_render
     dump_out.write((char*) chunk->indices, chunk->index_buffer_size * sizeof(uint32_t));
     dump_out.flush();
     dump_lock.unlock();
+#endif
 }
 
 void dump_remove_chunk_geometry_for_filter(const char* filter_name, mc_chunk_render_object * chunk) {
+#ifdef DUMP_NOVA_CALLS
     dump_lock.lock();
     DUMP_FUNC;
     dump_out.write((char*) filter_name, strlen(filter_name) + 1);
@@ -107,35 +131,45 @@ void dump_remove_chunk_geometry_for_filter(const char* filter_name, mc_chunk_ren
     dump_out.write((char*) chunk->indices, chunk->index_buffer_size * sizeof(uint32_t));
     dump_out.flush();
     dump_lock.unlock();
+#endif
 }
 
 void dump_execute_frame() {
+#ifdef DUMP_NOVA_CALLS
     dump_lock.lock();
     DUMP_FUNC;
     dump_lock.unlock();
+#endif
 }
 
 void dump_set_fullscreen(int fs) {
+#ifdef DUMP_NOVA_CALLS
     dump_lock.lock();
     DUMP_FUNC;
     W_UINT32(fs);
     dump_out.flush();
     dump_lock.unlock();
+#endif
 }
 
 void dump_should_close() {
+#ifdef DUMP_NOVA_CALLS
     dump_lock.lock();
     DUMP_FUNC;
     dump_lock.unlock();
+#endif
 }
 
 void dump_display_is_active() {
+#ifdef DUMP_NOVA_CALLS
     dump_lock.lock();
     DUMP_FUNC;
     dump_lock.unlock();
+#endif
 }
 
 void dump_add_gui_geometry(const char * geo_type, mc_gui_geometry * gui_geometry) {
+#ifdef DUMP_NOVA_CALLS
     dump_lock.lock();
     DUMP_FUNC;
     dump_out.write((char*) geo_type, strlen(geo_type) + 1);
@@ -147,33 +181,41 @@ void dump_add_gui_geometry(const char * geo_type, mc_gui_geometry * gui_geometry
     dump_out.write((char*) gui_geometry->vertex_buffer, gui_geometry->vertex_buffer_size * sizeof(float));
     dump_out.flush();
     dump_lock.unlock();
+#endif
 }
 
 void dump_clear_gui_buffers() {
+#ifdef DUMP_NOVA_CALLS
     dump_lock.lock();
     DUMP_FUNC;
     dump_lock.unlock();
+#endif
 }
 
 void dump_set_string_setting(const char * setting_name, const char * setting_value) {
+#ifdef DUMP_NOVA_CALLS
     dump_lock.lock();
     DUMP_FUNC;
     dump_out.write((char*) setting_name, strlen(setting_name) + 1);
     dump_out.write((char*) setting_value, strlen(setting_value) + 1);
     dump_out.flush();
     dump_lock.unlock();
+#endif
 }
 
 void dump_set_float_setting(const char * setting_name, float setting_value) {
+#ifdef DUMP_NOVA_CALLS
     dump_lock.lock();
     DUMP_FUNC;
     dump_out.write((char*) setting_name, strlen(setting_name) + 1);
     W_FLOAT(setting_value);
     dump_out.flush();
     dump_lock.unlock();
+#endif
 }
 
 void dump_set_player_camera_transform(double x, double y, double z, float yaw, float pitch) {
+#ifdef DUMP_NOVA_CALLS
     dump_lock.lock();
     DUMP_FUNC;
     W_DOUBLE(x);
@@ -183,73 +225,32 @@ void dump_set_player_camera_transform(double x, double y, double z, float yaw, f
     W_FLOAT(pitch);
     dump_out.flush();
     dump_lock.unlock();
+#endif
 }
 
 void dump_set_mouse_grabbed(int grabbed) {
+#ifdef DUMP_NOVA_CALLS
     dump_lock.lock();
     DUMP_FUNC;
     W_UINT32(grabbed);
     dump_out.flush();
     dump_lock.unlock();
+#endif
 }
 
 void dump_get_materials_and_filters() {
+#ifdef DUMP_NOVA_CALLS
     dump_lock.lock();
     DUMP_FUNC;
     dump_lock.unlock();
+#endif
 }
 
 void dump_destruct() {
+#ifdef DUMP_NOVA_CALLS
     dump_lock.lock();
     DUMP_FUNC;
     dump_lock.unlock();
     dump_out.close();
-}
-
-#else
-
-#define DUMP_FUNC
-#define W_UINT32(x)
-#define W_FLOAT(x)
-#define W_DOUBLE(x)
-
-void dump_initialize() {}
-
-void dump_add_texture(mc_atlas_texture* texture) {}
-
-void dump_reset_texture_manager() {}
-
-void dump_send_lightmap_texture(uint32_t* data, int count, int width, int height) {}
-
-void dump_add_texture_location(mc_texture_atlas_location* location) {}
-
-void dump_get_max_texture_size() {}
-
-void dump_add_chunk_geometry_for_filter(const char* filter_name, mc_chunk_render_object * chunk) {}
-
-void dump_remove_chunk_geometry_for_filter(const char* filter_name, mc_chunk_render_object * chunk) {}
-
-void dump_execute_frame() {}
-
-void dump_set_fullscreen(int fs) {}
-
-void dump_should_close() {}
-
-void dump_display_is_active() {}
-
-void dump_add_gui_geometry(const char * geo_type, mc_gui_geometry * gui_geometry) {}
-
-void dump_clear_gui_buffers() {}
-
-void dump_set_string_setting(const char * setting_name, const char * setting_value) {}
-
-void dump_set_float_setting(const char * setting_name, float setting_value) {}
-
-void dump_set_player_camera_transform(double x, double y, double z, float yaw, float pitch) {}
-
-void dump_set_mouse_grabbed(int grabbed) {}
-
-void dump_get_materials_and_filters() {}
-
-void dump_destruct() {}
 #endif
+}
