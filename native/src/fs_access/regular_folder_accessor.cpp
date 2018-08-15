8d713e6c115a9e7c693ebadfbf6eec605179cdd9
David Dubois <dexcelstraun7@gmail.com>
2018-08-15 00:26:23 -0700

    can read from files

--- native/src/fs_access/regular_folder_accessor.cpp
+++ native/src/fs_access/regular_folder_accessor.cpp
@@ -3,6 +3,7 @@
  * \date 14-Aug-18.
  */
 
+#include <fstream>
 #include "regular_folder_accessor.h"
 
 namespace nova {
@@ -28,7 +29,25 @@ namespace nova {
 
     std::vector<uint8_t> regular_folder_accessor::load_resource(const fs::path &resource_path) {
         if(!does_resource_exist(resource_path)) {
-            throw std::runtime_error("Resouce at path " + resource_path.string() + " does not exist");
+            throw std::runtime_error("Resource at path " + resource_path.string() + " does not exist");
         }
+
+        std::vector<uint8_t> buf;
+        std::ifstream resource_stream(resource_path.string());
+        if(!resource_stream.good()) {
+            // Error reading this file - it can't be read again in the future
+            const auto resource_string = resource_path.string();
+
+            resource_existance.emplace(resource_string, false);
+            throw std::runtime_error("Could not load resource at path " + resource_string);
+        }
+
+        while(!resource_stream.eof()) {
+            uint8_t val;
+            resource_stream >> val;
+            buf.push_back(val);
+        }
+
+        return buf;
     }
 }
