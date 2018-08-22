8dbca11a2a14a2734c867780127ba11c3ab46d8f
David Dubois <dexcelstraun7@gmail.com>
2018-08-21 19:49:30 -0700

    Added logger interface

---
+++ native/src/util/logger.cpp
@@ -0,0 +1,29 @@
+/*!
+ * \author ddubois 
+ * \date 21-Aug-18.
+ */
+
+#include "logger.hpp"
+
+namespace nova {
+    void logger::add_log_handler(log_level level, const std::function<void(std::string)> &log_handler) {
+        log_handlers.emplace(level, log_handler);
+    }
+
+    void logger::log(const log_level level, const std::string &msg) {
+        if(is_level_enabled.at(level)) {
+            log_handlers.at(level)(msg);
+        }
+    }
+
+    void logger::set_maximum_level_enabled(const log_level level, bool is_enabled) {
+        for(log_level i = log_level::TRACE; i < log_level::MAX_LEVEL) {
+            if(i < level) {
+                is_level_enabled.emplace(i, false);
+
+            } else {
+                is_level_enabled.emplace(i, true);
+            }
+        }
+    }
+}
---
+++ native/src/util/logger.hpp
@@ -0,0 +1,42 @@
+/*!
+ * \author ddubois 
+ * \date 21-Aug-18.
+ */
+
+#ifndef NOVA_RENDERER_LOGGER_HPP
+#define NOVA_RENDERER_LOGGER_HPP
+
+#include <string>
+#include <functional>
+#include <unordered_map>
+
+namespace nova {
+    enum class log_level {
+        TRACE,
+        DEBUG,
+        INFO,
+        WARN,
+        ERROR,
+        MAX_LEVEL
+    };
+
+    /*!
+     * \brief A logger interface that can be implemented for whatever game Nova uses
+     */
+    class logger {
+    public:
+        void add_log_handler(log_level level, const std::function<void(std::string)>& log_handler);
+
+        void log(log_level level, const std::string& msg);
+
+        void set_maximum_level_enabled(const log_level level, bool is_enabled);
+
+    private:
+        std::unordered_map<log_level, bool> is_level_enabled;
+
+        std::unordered_map<log_level, std::function<void(std::string)>> log_handlers;
+    };
+}
+
+
+#endif //NOVA_RENDERER_LOGGER_HPP
