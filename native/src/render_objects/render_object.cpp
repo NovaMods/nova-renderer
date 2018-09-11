3e5c80f1a67ee6eb1d8bba6b2113541c59d4eff3
Janrupf <werbung.janrupf@t-online.de>
2018-09-11 20:30:39 +0200

    Started working on render objects

---
+++ native/src/render_objects/render_object.cpp
@@ -0,0 +1,35 @@
+//
+// Created by jannis on 11.09.18.
+//
+
+#include "render_object.hpp"
+
+namespace nova {
+    render_object::render_object(const std::shared_ptr<render_object> &parent) : parent(parent) {
+        parent->children.push_back(shared_from_this());
+    }
+
+    render_object::~render_object() {
+        if(auto parent = get_parent()) {
+            parent->children.remove(shared_from_this());
+        }
+
+        for(auto &child : children) {
+            child.reset();
+        }
+    }
+
+    std::shared_ptr<render_object> render_object::get_parent() const {
+        return parent.lock();
+    }
+
+    const std::list<std::shared_ptr<render_object>> &render_object::get_children() const {
+        return children;
+    }
+
+    constexpr render_object &render_object::pos(const glm::vec3 &pos) {
+        _pos = pos;
+        return *this;
+    }
+
+}
