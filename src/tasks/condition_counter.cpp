109b704bef1410984120cba4e101d449b47c1d98
David Dubois <dexcelstraun7@gmail.com>
2018-12-18 20:02:18 -0800

    Finished updating nova_renderer.cpp!

---
+++ src/tasks/condition_counter.cpp
@@ -0,0 +1,6 @@
+/*!
+ * \author ddubois 
+ * \date 17-Dec-18.
+ */
+
+#include "condition_counter.hpp"
---
+++ src/tasks/condition_counter.hpp
@@ -0,0 +1,20 @@
+/*!
+ * \author ddubois 
+ * \date 17-Dec-18.
+ */
+
+#ifndef NOVA_RENDERER_CONDITION_COUNTER_HPP
+#define NOVA_RENDERER_CONDITION_COUNTER_HPP
+
+
+
+/*! 
+ * \brief
+ */
+class condition_counter {
+
+};
+
+
+
+#endif //NOVA_RENDERER_CONDITION_COUNTER_HPP
