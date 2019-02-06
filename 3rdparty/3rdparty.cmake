ea10c892ee0919dda10ce6b6ad46cd753d90c53c
David Dubois <dexcelstraun7@gmail.com>
2019-02-05 21:44:03 -0800

    Test code now adds mesh

--- 3rdparty/3rdparty.cmake
+++ 3rdparty/3rdparty.cmake
@@ -35,6 +35,7 @@ set(MINITRACE_SOURCE ${CMAKE_CURRENT_LIST_DIR}/minitrace/minitrace.c
                      ${CMAKE_CURRENT_LIST_DIR}/minitrace/minitrace.h)
 add_library(minitrace ${MINITRACE_SOURCE})
 target_include_directories(minitrace PUBLIC ${CMAKE_CURRENT_LIST_DIR}/minitrace)
+target_compile_definitions(minitrace PUBLIC MTR_ENABLED)
 
 
 set(SPIRV_SKIP_TESTS ON CACHE BOOL "Disable SPIRV-Tools tests" FORCE)
