0f6d209cc93e1947a98e936c6d796f7872287d72
Connor Wade Fitzgerald <connorwadefitzgerald@gmail.com>
2019-02-09 16:07:35 -0500

    Use two dashes in run-clang-tidy for long options

--- jenkins.sh
+++ jenkins.sh
@@ -46,7 +46,7 @@ cd ..
 
 # Linting
 cd build-clang
-${WORKSPACE}/3rdparty/run-clang-tidy/run-clang-tidy.py -export-fixes fixes.yaml -j8 -header-filter "${WORKSPACE}"'/(src|tests)/.*' `find ../{src,tests}/ -iname '*.cpp'` -clang-tidy-binary clang-tidy-8
+${WORKSPACE}/3rdparty/run-clang-tidy/run-clang-tidy.py --export-fixes fixes.yaml -j8 --header-filter "${WORKSPACE}"'/(src|tests)/.*' `find ../{src,tests}/ -iname '*.cpp'` --clang-tidy-binary clang-tidy-8
 # test `cat fixes.yaml | wc -c` -eq 1
 echo "End linting"
 cd ..
