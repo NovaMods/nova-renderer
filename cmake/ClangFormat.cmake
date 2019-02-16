fdcb247448316b6ad87ad54222a69e52fa60693e
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-02-16 06:16:56 -0500

    cmake: various clang-format fixes

--- cmake/ClangFormat.cmake
+++ cmake/ClangFormat.cmake
@@ -1,9 +1,3 @@
-if(__NOVA_CLANG_FORMAT_INCLUDE_GUARD)
-    return()
-else()
-    set(__NOVA_CLANG_FORMAT_INCLUDE_GUARD On)
-endif()
-
 if(NOT CLANG_FORMAT_COMMAND)
     set(CLANG_FORMAT_COMMAND "clang-format-9" "clang-format-8" "clang-format-7" "clang-format")
 endif()
@@ -12,7 +6,7 @@ find_program(CLANG_FORMAT_PROGRAM NAMES ${CLANG_FORMAT_COMMAND})
 
 execute_process(COMMAND "${CLANG_FORMAT_PROGRAM}" "--version"
                 OUTPUT_VARIABLE CLANG_TIDY_VERSION_FULL_STRING)
-string(REGEX MATCH [=[[0-9]\.[0-9].[0-9]]=] VERSION ${CLANG_TIDY_VERSION_FULL_STRING})
+string(REGEX MATCH [=[[0-9]\.[0-9].[0-9]]=] VERSION "${CLANG_TIDY_VERSION_FULL_STRING}")
 if(VERSION VERSION_LESS "7.0.0")
     message(STATUS "clang-format version ${VERSION} is not greater or equal to 7.0.0")
     set(CLANG_FORMAT_TERMINATE On)
@@ -26,13 +20,20 @@ if(CLANG_FORMAT_TERMINATE)
     function(format)
         # no-op
     endfunction()
+    function(nova_format)
+        # no-op
+    endfunction()
     return()
 endif()
 
 message(STATUS "Found clang-format at ${CLANG_FORMAT_PROGRAM}")
 
-add_custom_target(format VERBATIM)
-add_custom_target(reformat VERBATIM)
+if(NOT TARGET format)
+    add_custom_target(format VERBATIM)
+endif()
+if(NOT TARGET reformat)
+    add_custom_target(reformat VERBATIM)
+endif()
 
 file(GLOB_RECURSE CLANG_FORMAT_PATHS CONFIGURE_DEPENDS "*.clang-format")
 foreach(PATH ${CLANG_FORMAT_PATHS})
