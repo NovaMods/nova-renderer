425b73604710b72e2fec68d87f81627cd2c8ea98
David Dubois <dexcelstraun7@gmail.com>
2019-02-18 23:33:39 -0800

    Import ubo stuff

---
+++ src/render_engine/vulkan/uniform_buffer.cpp
@@ -0,0 +1,65 @@
+#include "uniform_buffer.hpp"
+#include <memory>
+#include "../../util/logger.hpp"
+
+namespace nova {
+
+    uniform_buffer::uniform_buffer(std::string name, const VmaAllocator allocator, VkBufferCreateInfo create_info, uint64_t min_alloc_size, bool mapped) : name(std::move(name)), min_alloc_size(min_alloc_size), allocator(allocator) {
+        VmaAllocationCreateInfo alloc_create = {};
+        alloc_create.usage = VMA_MEMORY_USAGE_CPU_TO_GPU;
+
+        if(mapped) {
+            alloc_create.flags = VMA_ALLOCATION_CREATE_MAPPED_BIT | VMA_ALLOCATION_CREATE_DEDICATED_MEMORY_BIT;
+        }
+
+        auto buffer_create_result = vmaCreateBuffer(allocator, reinterpret_cast<const VkBufferCreateInfo*>(&create_info), &alloc_create, reinterpret_cast<VkBuffer*>(&buffer), &allocation, &allocation_info);
+
+        if(buffer_create_result != VK_SUCCESS) {
+            NOVA_LOG(ERROR) << "Could not allocate a an autobuffer because " << buffer_create_result;
+        } else {
+            NOVA_LOG(TRACE) << "Auto buffer allocation success! Buffer ID: " << buffer;
+        }
+    }
+
+    uniform_buffer::uniform_buffer(uniform_buffer&& old) noexcept
+        : name(old.name), min_alloc_size(old.min_alloc_size), allocator(old.allocator), device(old.device), buffer(old.buffer), allocation(old.allocation), allocation_info(old.allocation_info) {
+
+        old.device = VkDevice{};
+        old.buffer = VkBuffer{};
+        old.allocation = {};
+        old.allocation_info = {};
+    }
+
+    uniform_buffer::~uniform_buffer() {
+        if(allocator && buffer != VkBuffer()) {
+            NOVA_LOG(TRACE) << "uniform_buffer: About to destroy buffer " << buffer;
+            vmaDestroyBuffer(allocator, buffer, allocation);
+        }
+    }
+
+    VmaAllocation& uniform_buffer::get_allocation() {
+        return allocation;
+    }
+
+    VmaAllocationInfo& uniform_buffer::get_allocation_info() {
+        return allocation_info;
+    }
+    const std::string& uniform_buffer::get_name() const {
+        return name;
+    }
+
+    const VkBuffer& uniform_buffer::get_vk_buffer() const {
+        return buffer;
+    }
+
+    const uint64_t uniform_buffer::get_size() const {
+        return min_alloc_size;
+    }
+
+    void uniform_buffer::set_data(const void* data, uint32_t size) {
+        void* mapped_data;
+        vmaMapMemory(allocator, allocation, &mapped_data);
+        std::memcpy(mapped_data, data, size);
+        vmaUnmapMemory(allocator, allocation);
+    }
+} // namespace nova
---
+++ src/render_engine/vulkan/uniform_buffer.hpp
@@ -0,0 +1,51 @@
+/*!
+ * \author David
+ * \date 22-May-16.
+ */
+
+#ifndef RENDERER_GL_UNIFORM_BUFFER_H
+#define RENDERER_GL_UNIFORM_BUFFER_H
+
+#include <vk_mem_alloc.h>
+#include <string>
+
+namespace nova {
+    /*!
+     * \brief A nice interface for uniform buffer objects
+     */
+    class uniform_buffer {
+    public:
+        uniform_buffer() = default;
+
+        uniform_buffer(std::string name, const VmaAllocator allocation, VkBufferCreateInfo create_info, uint64_t min_alloc_size, bool mapped);
+
+        uniform_buffer(uniform_buffer&& old) noexcept;
+
+        virtual ~uniform_buffer();
+
+        VmaAllocation& get_allocation();
+        VmaAllocationInfo& get_allocation_info();
+
+        void set_data(const void* data, uint32_t size);
+
+        const std::string& get_name() const;
+
+        const VkBuffer& get_vk_buffer() const;
+
+        const uint64_t get_size() const;
+
+    protected:
+        std::string name;
+        uint64_t min_alloc_size;
+
+        const VmaAllocator allocator;
+
+        VkDevice device;
+        VkBuffer buffer;
+
+        VmaAllocation allocation;
+        VmaAllocationInfo allocation_info;
+    };
+} // namespace nova
+
+#endif // RENDERER_GL_UNIFORM_BUFFER_H
