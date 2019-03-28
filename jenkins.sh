73df3f8714823739e6005f19f7006410b2e498b5
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-03-28 01:59:55 -0400

    Error formatting on PRs, sigh on branches

--- jenkins.sh
+++ jenkins.sh
@@ -76,20 +76,21 @@ echo "End formatting"
 if [ `git status --porcelain src tests | wc -c` -eq 0 ]; then
     echo "No formatting errors found."
 else
+    git diff src tests
     # Only for branches
     if [[ $GIT_BRANCH == *'origin'* ]]; then
-        LOCAL_BRANCH=$(echo $GIT_BRANCH | sed -e 's/origin\///g')
-        echo "Formatting errors found. Committing changes."
-        git remote add origin-ssh git@github.com:/NovaMods/nova-renderer.git || true
-        git stash
-        git checkout `echo $LOCAL_BRANCH | sed -e 's/origin\///g'`
-        git stash pop
-        git pull --ff
-        git add src tests
-        git commit -m "Automated formatting correction [ci skip]"
-        git push origin-ssh $LOCAL_BRANCH
+        echo "Formatting errors found. Meh. Gib formatting pls."
+        #LOCAL_BRANCH=$(echo $GIT_BRANCH | sed -e 's/origin\///g')
+        #echo "Formatting errors found. Committing changes."
+        #git remote add origin-ssh git@github.com:/NovaMods/nova-renderer.git || true
+        #git stash
+        #git checkout `echo $LOCAL_BRANCH | sed -e 's/origin\///g'`
+        #git stash pop
+        #git pull --ff
+        #git add src tests
+        #git commit -m "Automated formatting correction [ci skip]"
+        #git push origin-ssh $LOCAL_BRANCH
     else
-        git status src tests
         echo "Formatting errors found. Quitting."
         exit 1
     fi
