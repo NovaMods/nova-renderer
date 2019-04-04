93ce7ca98cdfd5af384dfa45160d52d099e5d4bf
David Dubois <dexcelstraun7@gmail.com>
2019-04-03 23:50:22 -0700

    Direct3D 12 implementaiton of render pass creation

--- src/render_engine/dx12/d3d12_render_engine.hpp
+++ src/render_engine/dx12/d3d12_render_engine.hpp
@@ -1,18 +1,52 @@
 /*!
- * \author ddubois 
+ * \author ddubois
  * \date 03-Apr-19.
  */
 
-#ifndef NOVA_RENDERER_D_2_D_12_RENDER_ENGINE_HPP
-#define NOVA_RENDERER_D3D12_RENDER_ENGINE_HPP
+#pragma once
 
+#include <nova_renderer/render_engine.hpp>
 
-/*!
- * \brief
- */
-class d2d12_render_engine {
+namespace nova::renderer {
+    struct d3d12_renderpass_t : renderpass_t{};
 
-};
+    /*!
+     * \brief D3D12 implmentation of a render engine
+     */
+    class d2d12_render_engine : public render_engine {
+    public:
+        d2d12_render_engine(nova_settings& settings);
 
+        d2d12_render_engine(d2d12_render_engine&& old) noexcept = delete;
+        d2d12_render_engine& operator=(d2d12_render_engine&& old) noexcept = delete;
 
-#endif //NOVA_RENDERER_D_2_D_12_RENDER_ENGINE_HPP
+        d2d12_render_engine(const d2d12_render_engine& other) = delete;
+        d2d12_render_engine& operator=(const d2d12_render_engine& other) = delete;
+
+        // Inherited via render_engine
+        virtual std::shared_ptr<window> get_window() const override;
+        virtual result<renderpass_t*> create_renderpass(const render_pass_create_info_t& data) override;
+        virtual framebuffer_t* create_framebuffer(const std::vector<resource_t*>& attachments) override;
+        virtual pipeline_t* create_pipeline(const pipeline_create_info_t& data) override;
+        virtual resource_t* create_buffer(const buffer_create_info_t& info) override;
+        virtual resource_t* create_texture(const texture2d_create_info_t& info) override;
+        virtual semaphore_t* create_semaphore() override;
+        virtual std::vector<semaphore_t*> create_semaphores(uint32_t num_semaphores) override;
+        virtual fence_t* create_fence(bool signaled = false) override;
+        virtual std::vector<fence_t*> create_fences(uint32_t num_fences, bool signaled = false) override;
+        virtual void destroy_renderpass(renderpass_t* pass) override;
+        virtual void destroy_pipeline(pipeline_t* pipeline) override;
+        virtual void destroy_resource(resource_t* resource) override;
+        virtual void destroy_semaphores(const std::vector<semaphore_t*>& semaphores) override;
+        virtual void destroy_fences(const std::vector<fence_t*>& fences) override;
+        virtual command_list_t* allocate_command_list(uint32_t thread_idx,
+                                                      queue_type needed_queue_type,
+                                                      command_list_t::level level) override;
+        virtual void submit_command_list(command_list_t* cmds,
+                                         queue_type queue,
+                                         fence_t* fence_to_signal = nullptr,
+                                         const std::vector<semaphore_t*>& wait_semaphores = {},
+                                         const std::vector<semaphore_t*>& signal_semaphores = {}) override;
+        virtual void open_window(uint32_t width, uint32_t height) override;
+    };
+} // namespace nova::renderer
