75b82767d1adc8110d5bc9e925ccf1f2f4380edf
David Dubois <dexcelstraun7@gmail.com>
2019-04-03 23:45:12 -0700

    OpenGL implementation of renderpass creation

--- src/render_engine/gl2/gl2_render_engine.cpp
+++ src/render_engine/gl2/gl2_render_engine.cpp
@@ -26,86 +26,26 @@ namespace nova::renderer {
         glFrontFace(GL_CCW);
     }
 
-    void gl2_render_engine::set_shaderpack(const shaderpack_data_t& data) {
-        destroy_dynamic_textures();
-
-        create_dynamic_textures(data.resources.textures);
-        for(const sampler_create_info_t& sampler_data : data.resources.samplers) {
-            samplers.emplace(sampler_data.name, sampler_data);
-        }
+    result<renderpass_t*> gl2_render_engine::create_renderpass(const render_pass_create_info_t& data) {
+        return result<renderpass_t*>(new renderpass_t);
     }
 
-    command_list_t* gl2_render_engine::allocate_command_list([[maybe_unused]] uint32_t thread_idx,
-                                                           [[maybe_unused]] queue_type needed_queue_type,
-                                                           [[maybe_unused]] command_list_t::level command_list_type) {
-        return new gl2_command_list();
-    }
-
-    void gl2_render_engine::create_dynamic_textures(const std::vector<texture_create_into_t>& texture_datas) {
-        dynamic_textures.reserve(texture_datas.size());
-
-        std::vector<GLuint> texture_ids(texture_datas.size());
-        glGenTextures(texture_ids.size(), texture_ids.data());
-
-        for(const texture_create_into_t& data : texture_datas) {
-            gl_texture texture = {};
-            texture.id = texture_ids.front();
-            texture_ids.pop_back();
-
-            glBindTexture(GL_TEXTURE_2D, texture.id);
-
-            GLenum internal_format = GL_RGBA8;
-            GLenum format = GL_RGBA;
-            GLenum type = GL_UNSIGNED_BYTE;
-
-            switch(data.format.pixel_format) {
-                case pixel_format_enum::RGBA8:
-                    internal_format = GL_RGBA8;
-                    format = GL_RGBA;
-                    type = GL_UNSIGNED_BYTE;
-                    break;
-
-                case pixel_format_enum::RGBA32F:
-                    NOVA_LOG(WARN)
-                        << "You requested a texture with 128 bits per component, but your graphics card only supports 64 bits per component";
-                case pixel_format_enum::RGBA16F:
-                    internal_format = GL_RGBA16;
-                    format = GL_RGBA;
-                    type = GL_UNSIGNED_SHORT;
-                    break;
-
-                case pixel_format_enum::Depth:
-                    internal_format = GL_DEPTH_COMPONENT32;
-                    format = GL_RED;
-                    type = GL_UNSIGNED_INT;
-                    break;
-
-                case pixel_format_enum::DepthStencil:
-                    internal_format = GL_DEPTH_COMPONENT24;
-                    format = GL_RED;
-                    type = GL_UNSIGNED_SHORT;
-                    break;
-            }
-
-            const glm::uvec2 texture_size = data.format.get_size_in_pixels(window->get_window_size());
-
-            glTexImage2D(GL_TEXTURE_2D, 0, internal_format, texture_size.x, texture_size.y, 0, format, type, nullptr);
-
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-
-            dynamic_textures.emplace(data.name, texture);
-        }
-    }
-
-    void gl2_render_engine::destroy_dynamic_textures() {
-        std::vector<GLuint> textures_to_delete;
-        textures_to_delete.reserve(dynamic_textures.size());
-        for(const auto& [texture_name, texture] : dynamic_textures) {
-            textures_to_delete.push_back(texture.id);
-        }
-
-        glDeleteTextures(textures_to_delete.size(), textures_to_delete.data());
-        dynamic_textures.clear();
-    }
+    framebuffer_t* gl2_render_engine::create_framebuffer(const std::vector<resource_t*>& attachments) { return nullptr; }
+    pipeline_t* gl2_render_engine::create_pipeline(const pipeline_create_info_t& data) { return nullptr; }
+    resource_t* gl2_render_engine::create_buffer(const buffer_create_info_t& info) { return nullptr; }
+    resource_t* gl2_render_engine::create_texture(const texture2d_create_info_t& info) { return nullptr; }
+    semaphore_t* gl2_render_engine::create_semaphore() { return nullptr; }
+    std::vector<semaphore_t*> gl2_render_engine::create_semaphores(uint32_t num_semaphores) { return std::vector<semaphore_t*>(); }
+    fence_t* gl2_render_engine::create_fence(bool signaled = false) { return nullptr; }
+    std::vector<fence_t*> gl2_render_engine::create_fences(uint32_t num_fences, bool signaled = false) { return std::vector<fence_t*>(); }
+    void gl2_render_engine::destroy_renderpass(renderpass_t* pass) {}
+    void gl2_render_engine::destroy_pipeline(pipeline_t* pipeline) {}
+    void gl2_render_engine::destroy_resource(resource_t* resource) {}
+    void gl2_render_engine::destroy_semaphores(const std::vector<semaphore_t*>& semaphores) {}
+    void gl2_render_engine::destroy_fences(const std::vector<fence_t*>& fences) {}
+    void gl2_render_engine::submit_command_list(command_list_t* cmds,
+                                                queue_type queue,
+                                                fence_t* fence_to_signal = nullptr,
+                                                const std::vector<semaphore_t*>& wait_semaphores = {},
+                                                const std::vector<semaphore_t*>& signal_semaphores = {}) {}
 } // namespace nova::renderer
--- src/render_engine/gl2/gl2_render_engine.hpp
+++ src/render_engine/gl2/gl2_render_engine.hpp
@@ -1,5 +1,5 @@
 /*!
- * \author ddubois 
+ * \author ddubois
  * \date 31-Mar-19.
  */
 
@@ -8,9 +8,13 @@
 #include "glad.h"
 
 namespace nova::renderer {
-    struct gl_texture {
+    struct gl_resource_t : resource_t {
         GLuint id;
     };
+
+	struct gl_renderpass_t : renderpass_t {
+
+	};
 
     /*!
      * \brief OpenGL 2.1 render engine because compatibility
@@ -28,38 +32,38 @@ namespace nova::renderer {
         ~gl2_render_engine() override;
 
         std::shared_ptr<window> get_window() const override;
-        
-        void set_shaderpack(const shaderpack_data_t& data) override;
 
-        result<renderable_id_t> add_renderable(const static_mesh_renderable_data& data) override;
-
-        void set_renderable_visibility(renderable_id_t id, bool is_visible) override;
-
-        void delete_renderable(renderable_id_t id) override;
-
-        result<mesh_id_t> add_mesh(const mesh_data& mesh) override;
-
-        void delete_mesh(uint32_t mesh_id) override;
-
-        command_list_t* allocate_command_list(uint32_t thread_idx, queue_type needed_queue_type, command_list_t::level command_list_type) override;
-
-        void render_frame() override;
+        // Inherited via render_engine
+        virtual result<renderpass_t*> create_renderpass(const render_pass_create_info_t& data) override;
+        virtual framebuffer_t* create_framebuffer(const std::vector<resource_t*>& attachments) override;
+        virtual pipeline_t* create_pipeline(const pipeline_create_info_t& data) override;
+        virtual resource_t* create_buffer(const buffer_create_info_t& info) override;
+        virtual resource_t* create_texture(const texture2d_create_info_t& info) override;
+        virtual semaphore_t* create_semaphore() override;
+        virtual std::vector<semaphore_t*> create_semaphores(uint32_t num_semaphores) override;
+        virtual fence_t* create_fence(bool signaled = false) override;
+        virtual std::vector<fence_t*> create_fences(uint32_t num_fences, bool signaled = false) override;
+        virtual void destroy_renderpass(renderpass_t* pass) override;
+        virtual void destroy_pipeline(pipeline_t* pipeline) override;
+        virtual void destroy_resource(resource_t* resource) override;
+        virtual void destroy_semaphores(const std::vector<semaphore_t*>& semaphores) override;
+        virtual void destroy_fences(const std::vector<fence_t*>& fences) override;
+        command_list_t* allocate_command_list(uint32_t thread_idx,
+                                              queue_type needed_queue_type,
+                                              command_list_t::level command_list_type) override;
+        virtual void submit_command_list(command_list_t* cmds,
+                                         queue_type queue,
+                                         fence_t* fence_to_signal = nullptr,
+                                         const std::vector<semaphore_t*>& wait_semaphores = {},
+                                         const std::vector<semaphore_t*>& signal_semaphores = {}) override;
 
     protected:
         static void set_initial_state();
 
-#pragma region Window
         std::unique_ptr<window> window;
 
         void open_window(uint32_t width, uint32_t height) override;
-#pragma endregion
 
-#pragma region Shaderpack
-        std::unordered_map<std::string, gl_texture> dynamic_textures;
         std::unordered_map<std::string, sampler_create_info_t> samplers;
-
-        void create_dynamic_textures(const std::vector<texture_create_into_t>& texture_datas);
-        void destroy_dynamic_textures();
-#pragma endregion 
     };
-}
+} // namespace nova::renderer
