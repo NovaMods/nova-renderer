a3ec9802ad0e1ab5f6d7332fd487c7fbebf18a11
David Dubois <dexcelstraun7@gmail.com>
2019-04-03 23:50:22 -0700

    Direct3D 12 implementaiton of render pass creation

--- src/render_engine/dx12/d3d12_render_engine.cpp
+++ src/render_engine/dx12/d3d12_render_engine.cpp
@@ -4,3 +4,57 @@
  */
 
 #include "d3d12_render_engine.hpp"
+
+namespace nova::renderer {
+    d2d12_render_engine::d2d12_render_engine(nova_settings& settings) : render_engine(settings) {}
+
+    std::shared_ptr<window> d2d12_render_engine::get_window() const { return std::shared_ptr<window>(); }
+
+    result<renderpass_t*> d2d12_render_engine::create_renderpass(const render_pass_create_info_t& data) {
+        return result<renderpass_t*>(new d3d12_renderpass_t);
+    }
+
+    framebuffer_t* d2d12_render_engine::create_framebuffer(const std::vector<resource_t*>& attachments) { return nullptr; }
+
+    pipeline_t* d2d12_render_engine::create_pipeline(const pipeline_create_info_t& data) { return nullptr; }
+
+    resource_t* d2d12_render_engine::create_buffer(const buffer_create_info_t& info) { return nullptr; }
+
+    resource_t* d2d12_render_engine::create_texture(const texture2d_create_info_t& info) { return nullptr; }
+
+    semaphore_t* d2d12_render_engine::create_semaphore() { return nullptr; }
+
+    std::vector<semaphore_t*> d2d12_render_engine::create_semaphores(uint32_t num_semaphores) {
+        return std::vector<semaphore_t*>();
+    }
+
+    fence_t* d2d12_render_engine::create_fence(bool signaled = false) { return nullptr; }
+
+    std::vector<fence_t*> d2d12_render_engine::create_fences(uint32_t num_fences, bool signaled = false) {
+        return std::vector<fence_t*>();
+    }
+
+    void d2d12_render_engine::destroy_renderpass(renderpass_t* pass) {}
+
+    void d2d12_render_engine::destroy_pipeline(pipeline_t* pipeline) {}
+
+    void d2d12_render_engine::destroy_resource(resource_t* resource) {}
+
+    void d2d12_render_engine::destroy_semaphores(const std::vector<semaphore_t*>& semaphores) {}
+
+    void d2d12_render_engine::destroy_fences(const std::vector<fence_t*>& fences) {}
+
+    command_list_t* d2d12_render_engine::allocate_command_list(uint32_t thread_idx,
+                                                                               queue_type needed_queue_type,
+                                                                               command_list_t::level level) {
+        return nullptr;
+    }
+
+    void d2d12_render_engine::submit_command_list(command_list_t* cmds,
+                                                                  queue_type queue,
+                                                                  fence_t* fence_to_signal = nullptr,
+                                                                  const std::vector<semaphore_t*>& wait_semaphores = {},
+                                                                  const std::vector<semaphore_t*>& signal_semaphores = {}) {}
+
+    void d2d12_render_engine::open_window(uint32_t width, uint32_t height) {}
+} // namespace nova::renderer
