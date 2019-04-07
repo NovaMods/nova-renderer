fecb4205c4a9ab4b543f71e08086c1fcbba10428
David Dubois <dexcelstraun7@gmail.com>
2019-04-07 15:58:48 -0700

    D3D12 implementation of creating and deleting a texture

--- src/render_engine/dx12/dx12_utils.hpp
+++ src/render_engine/dx12/dx12_utils.hpp
@@ -10,16 +10,15 @@
 namespace nova::renderer::rhi {
     D3D12_RESOURCE_STATES to_dx12_state(resource_state state);
 
-    enum class pixel_format_enum;
-    DXGI_FORMAT to_dxgi_format(pixel_format_enum pixel_format);
+    DXGI_FORMAT to_dxgi_format(shaderpack::pixel_format_enum pixel_format);
 
-    enum D3D12_BLEND to_dx12_blend(blend_factor_enum blend_factor);
+    enum D3D12_BLEND to_dx12_blend(shaderpack::blend_factor_enum blend_factor);
 
-    D3D12_COMPARISON_FUNC to_dx12_compare_func(compare_op_enum depth_func);
+    D3D12_COMPARISON_FUNC to_dx12_compare_func(shaderpack::compare_op_enum depth_func);
 
-    D3D12_STENCIL_OP to_dx12_stencil_op(stencil_op_enum op);
+    D3D12_STENCIL_OP to_dx12_stencil_op(shaderpack::stencil_op_enum op);
 
-    D3D12_PRIMITIVE_TOPOLOGY_TYPE to_dx12_topology(primitive_topology_enum primitive_mode);
+    D3D12_PRIMITIVE_TOPOLOGY_TYPE to_dx12_topology(shaderpack::primitive_topology_enum primitive_mode);
 
 #ifndef NDEBUG
 #define CHECK_ERROR(expr, msg)                                                                                                             \
