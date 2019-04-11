199c7eaee623c5bb09c2367455ce828fb5ed96dd
David Dubois <dexcelstraun7@gmail.com>
2019-04-10 19:47:53 -0700

    Include window system headers for init

--- src/render_engine/vulkan/vulkan_render_engine_init.cpp
+++ src/render_engine/vulkan/vulkan_render_engine_init.cpp
@@ -2,6 +2,12 @@
 
 #include <fmt/format.h>
 
+#if _WIN32
+#define VK_USE_PLATFORM_WIN32_KHR
+#elif defined(linux) || defined(__linux) || defined(__linux__)
+#define VK_USE_PLATFORM_XLIB_KHR
+#endif
+
 #include "../../util/logger.hpp"
 #include "swapchain.hpp"
 #include "vulkan_render_engine.hpp"
