b051f3b9b4d3128884f5df8ca770143373dea6d0
David Dubois <dexcelstraun7@gmail.com>
2019-05-04 17:10:40 -0700

    Quick fix to OpenGL descriptors

--- src/render_engine/gl3/gl3_structs.hpp
+++ src/render_engine/gl3/gl3_structs.hpp
@@ -1,28 +1,27 @@
 /*!
  * \brief Gl2 definitions of the structs forward-declared in render_engine.hpp
  *
- * \author ddubois 
+ * \author ddubois
  * \date 01-Apr-19.
  */
 
 #pragma once
-#include "glad/glad.h"
-#include "nova_renderer/rhi_types.hpp"
 #include <string>
 #include <unordered_map>
+#include "glad/glad.h"
+#include "nova_renderer/rhi_types.hpp"
 
 namespace nova::renderer::rhi {
     // Holds all the state for an OpenGL sampler object, which may or may not be an actual thing in 3.1
-    struct Gl3Sampler {
-    };
+    struct Gl3SamplerState {};
 
-    struct Gl3Image : Image {
+    struct Gl3Resource {
         GLuint id = 0;
     };
 
-    struct Gl3Buffer : Buffer {
-        GLuint id = 0;
-    };
+    struct Gl3Image : Image, Gl3Resource {};
+
+    struct Gl3Buffer : Buffer, Gl3Resource {};
 
     struct Gl3Renderpass : Renderpass {};
 
@@ -36,17 +35,21 @@ namespace nova::renderer::rhi {
      * - Create descriptor set based off of interface
      * - Update descriptor set with resources
      * - Bind descriptor set in command buffer
-     * 
+     *
      * OpenGL descriptor sets need to hold a pointer to the resource they were updated with
      */
 
+    struct Gl3Descriptor {
+        Gl3Resource* resource = nullptr;
+    };
+
     struct Gl3DescriptorSet : DescriptorSet {
         // The index in the array is the binding in the set
-        std::vector<Resource*> resources;
+        std::vector<Gl3Descriptor> descriptor;
     };
 
     struct Gl3SamplerDescriptor : DescriptorSet {
-        Gl3Sampler sampler;
+        Gl3SamplerState sampler;
     };
 
     struct Gl3DescriptorPool : DescriptorPool {
@@ -59,8 +62,8 @@ namespace nova::renderer::rhi {
     };
 
     struct Gl3Pipeline : Pipeline {
-		GLuint id = 0;
+        GLuint id = 0;
 
-		std::unordered_map<ResourceBindingDescription, GLuint> uniform_cache;
+        std::unordered_map<ResourceBindingDescription, GLuint> uniform_cache;
     };
-}
+} // namespace nova::renderer::rhi
