dc36f316257b3f1f9edefa4433aff9e8d75a60c5
David Dubois <dexcelstraun7@gmail.com>
2019-05-14 22:47:39 -0700

    Pulling in eastl because allocators

--- setup-linux.sh
+++ setup-linux.sh
@@ -2,4 +2,4 @@
 
 vcpkg update
 vcpkg upgrade --no-dry-run
-vcpkg install --recurse miniz:x64-linux fmt:x64-linux glm:x64-linux glslang:x64-linux gtest:x64-linux nlohmann-json:x64-linux spirv-cross:x64-linux spirv-tools:x64-linux vulkan-memory-allocator:x64-linux vulkan:x64-linux spirv-tools:x64-linux minitrace:x64-linux
+vcpkg install --recurse miniz:x64-linux fmt:x64-linux glm:x64-linux glslang:x64-linux gtest:x64-linux nlohmann-json:x64-linux spirv-cross:x64-linux spirv-tools:x64-linux vulkan-memory-allocator:x64-linux vulkan:x64-linux spirv-tools:x64-linux minitrace:x64-linux eastl:x64-linux
--- setup-windows.ps1
+++ setup-windows.ps1
@@ -1,3 +1,3 @@
 vcpkg update
 vcpkg upgrade --no-dry-run
-vcpkg install --recurse miniz:x64-windows fmt:x64-windows glm:x64-windows glslang:x64-windows gtest:x64-windows nlohmann-json:x64-windows spirv-cross:x64-windows spirv-tools:x64-windows vulkan-memory-allocator:x64-windows vulkan:x64-windows spirv-tools:x64-windows minitrace:x64-windows
+vcpkg install --recurse miniz:x64-windows fmt:x64-windows glm:x64-windows glslang:x64-windows gtest:x64-windows nlohmann-json:x64-windows spirv-cross:x64-windows spirv-tools:x64-windows vulkan-memory-allocator:x64-windows vulkan:x64-windows spirv-tools:x64-windows minitrace:x64-windows eastl:x64-windows
