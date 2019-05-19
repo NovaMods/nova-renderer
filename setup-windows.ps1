a40093bd27b5557dbcf1ef7bc196fbcc678d5103
David Dubois <dexcelstraun7@gmail.com>
2019-05-18 17:33:02 -0700

    Updated to better follow new repo standards

--- setup-windows.ps1
+++
@@ -1,3 +0,0 @@
-vcpkg update
-vcpkg upgrade --no-dry-run
-vcpkg install --recurse miniz:x64-windows fmt:x64-windows glm:x64-windows glslang:x64-windows gtest:x64-windows nlohmann-json:x64-windows spirv-cross:x64-windows spirv-tools:x64-windows vulkan-memory-allocator:x64-windows vulkan:x64-windows spirv-tools:x64-windows minitrace:x64-windows eastl:x64-windows
