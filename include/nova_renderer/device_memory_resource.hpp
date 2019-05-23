b77b6b7f2597acf4aa04d8bc3eb479f278c6e642
David Dubois <dexcelstraun7@gmail.com>
2019-05-22 19:55:40 -0700

    More better device memory allocator

--- include/nova_renderer/device_memory_resource.hpp
+++ include/nova_renderer/device_memory_resource.hpp
@@ -3,6 +3,7 @@
 #include <bvestl/polyalloc/polyalloc.hpp>
 #include <foundational/allocation/bytes.hpp>
 #include <foundational/allocation/allocation_structs.hpp>
+#include <foundational/allocation/size_only_allocator.hpp>
 
 using namespace foundational::allocation::operators;
 
@@ -17,23 +18,24 @@ namespace nova::renderer {
      * \brief Couples an allocation strategy with a device memory object, allowing you to subdivide the device memory for use in individual
      * buffers, textures, etc
      */
-    template <typename AllocationStrategy>
     class DeviceMemoryResource {
     public:
+        template <typename AllocationStrategy>
         DeviceMemoryResource(struct DeviceMemory* memory,
                              bvestl::polyalloc::allocator_handle& host_allocator,
                              const foundational::allocation::Bytes size,
                              const foundational::allocation::Bytes alignment = 0_b)
-            : allocation_strategy(host_allocator, size, alignment), memory(memory) {}
-
-        DeviceMemoryAllocation allocate(foundational::allocation::Bytes size) { 
-            const foundational::allocation::AllocationInfo alloc_info = allocation_strategy.allocate(size);
-
-            return {memory, alloc_info};
+            : host_allocator(host_allocator), memory(memory) {
+            void* alloc_strat = host_allocator.allocate(sizeof(AllocationStrategy));
+            allocation_strategy = new(alloc_strat) AllocationStrategy(host_allocator, size, alignment);
         }
 
+        [[nodiscard]] DeviceMemoryAllocation allocate(const foundational::allocation::Bytes size) const;
+
     private:
-        AllocationStrategy allocation_strategy;
+        bvestl::polyalloc::allocator_handle& host_allocator;
+
+        foundational::allocation::SizeOnlyAllocator* allocation_strategy;
 
         struct DeviceMemory* memory;
     };
