9a8fa57164c5d901046412fb221dce17a7c5c42d
David Dubois <dexcelstraun7@gmail.com>
2019-10-23 08:20:29 -0700

    [vulkan] Vulkan includes Windows things

--- src/render_engine/vulkan/vulkan.hpp
+++ src/render_engine/vulkan/vulkan.hpp
@@ -2,15 +2,22 @@
 
 #include "nova_renderer/util/platform.hpp"
 
+#include <vulkan/vulkan.h>
+
+// I really don't know how Khronos/anyone else gets vulkan.h to work. Doing this manually feels dirty, and not in a
+// good way, but it works 
 #ifdef NOVA_LINUX
 #define VK_USE_PLATFORM_XLIB_KHR
+#include <X11/Xlib.h>
+#include <vulkan/vulkan_xlib.h>
+
 #elif defined(NOVA_WINDOWS)
 #define VK_USE_PLATFORM_WIN32_KHR
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
+#include <windows.h>
+#include <vulkan/vulkan_win32.h>
 #endif
-
-#include <vulkan/vulkan.h>
 
 // Thank you, Windows, for being an idiot
 #ifdef ERROR
