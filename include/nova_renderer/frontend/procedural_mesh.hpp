6b87f51ac1356bc890a0f3d8202af6f76011b72d
David Dubois <dexcelstraun7@gmail.com>
2019-12-01 22:28:26 -0800

    [frontend] Methods in ProceduralMesh

--- include/nova_renderer/frontend/procedural_mesh.hpp
+++ include/nova_renderer/frontend/procedural_mesh.hpp
@@ -1,15 +1,16 @@
 #pragma once
 
-#include <cstdint>
 #include <array>
+#include <cstdint>
 
 #include "nova_renderer/constants.hpp"
 
 namespace nova::renderer {
     namespace rhi {
+        class CommandList;
         class RenderEngine;
         class Buffer;
-    }
+    } // namespace rhi
 
     /*!
      * \brief ProceduralMesh is a mesh which the user will modify every frame
@@ -19,15 +20,57 @@ namespace nova::renderer {
      */
     class ProceduralMesh {
     public:
-        ProceduralMesh(uint64_t vertex_buffer_size, uint64_t index_buffer_size, rhi::RenderEngine& render_engine);
+        /*!
+         * \brief Creates a new procedural mesh which has the specified amount of space
+         *
+         * \param vertex_buffer_size The number of bytes the vertex buffer needs
+         * \param index_buffer_size The number of bytes that the index buffer needs
+         * \param device The device to create the buffers on
+         */
+        ProceduralMesh(uint64_t vertex_buffer_size, uint64_t index_buffer_size, rhi::RenderEngine& device);
+
+        /*!
+         * \brief Sets the data to upload to the vertex buffer
+         *
+         * \param data A pointer to the start of the data
+         * \param size The number of bytes to upload
+         */
+        void set_vertex_data(const void* data, uint64_t size);
+
+        /*!
+         * \brief Sets the data to upload to the index buffer
+         *
+         * \param data A pointer to the start of the data
+         * \param size The number of bytes to upload
+         */
+        void set_index_data(const void* data, uint64_t size);
+
+        /*!
+         * \brief Records commands to copy the staging buffers to the device buffers for the specified frame
+         *
+         * Intent is that you call this method at the beginning or the frame that will use the buffers you're uploading to. It's a method on
+         * this class mostly because writing accessors is hard
+         *
+         * \param cmds The command list to record commands into
+         * \param frame_idx The index of the frame to write the data to
+         */
+        void record_commands_to_upload_data(rhi::CommandList* cmds, uint8_t frame_idx);
 
     private:
         rhi::RenderEngine& render_engine;
 
         std::array<rhi::Buffer*, NUM_IN_FLIGHT_FRAMES> vertex_buffers;
-		std::array<rhi::Buffer*, NUM_IN_FLIGHT_FRAMES> index_buffers;
+        std::array<rhi::Buffer*, NUM_IN_FLIGHT_FRAMES> index_buffers;
 
         rhi::Buffer* cached_vertex_buffer;
         rhi::Buffer* cached_index_buffer;
+
+        uint64_t num_vertex_bytes_to_upload = 0;
+        uint64_t num_index_bytes_to_upload = 0;
+
+#ifndef NDEBUG
+        uint64_t vertex_buffer_size;
+        uint64_t index_buffer_size;
+#endif
     };
 } // namespace nova::renderer
--- src/render_objects/procedural_mesh.cpp
+++ src/render_objects/procedural_mesh.cpp
@@ -10,15 +10,21 @@
 namespace nova::renderer {
     using namespace rhi;
 
-    ProceduralMesh::ProceduralMesh(uint64_t vertex_buffer_size, uint64_t index_buffer_size, rhi::RenderEngine& render_engine)
-        : render_engine(render_engine) {
+    ProceduralMesh::ProceduralMesh(uint64_t vertex_buffer_size, uint64_t index_buffer_size, RenderEngine& device)
+        : render_engine(device)
+#ifndef NDEBUG
+          ,
+          vertex_buffer_size(vertex_buffer_size),
+          index_buffer_size(index_buffer_size)
+#endif
+    {
         const auto aligned_vertex_buffer_size = bvestl::polyalloc::align(vertex_buffer_size, 256);
         const auto aligned_index_buffer_size = bvestl::polyalloc::align(index_buffer_size, 256);
         const auto host_memory_size = aligned_vertex_buffer_size + aligned_index_buffer_size;
         const auto device_memory_size = host_memory_size * 3;
 
         // TODO: Don't allocate a separate device memory for each procedural mesh
-        render_engine.allocate_device_memory(device_memory_size.b_count(), MemoryUsage::LowFrequencyUpload, ObjectType::Buffer)
+        device.allocate_device_memory(device_memory_size.b_count(), MemoryUsage::LowFrequencyUpload, ObjectType::Buffer)
             .map([&](DeviceMemory* memory) {
                 // TODO: Find a good way to keep these around
                 const auto
@@ -28,21 +34,22 @@ namespace nova::renderer {
 
                 const auto vertex_create_info = BufferCreateInfo{vertex_buffer_size, BufferUsage::VertexBuffer};
                 for(auto& vertex_buffer : vertex_buffers) {
-                    vertex_buffer = render_engine.create_buffer(vertex_create_info, memory_resource);
+                    vertex_buffer = device.create_buffer(vertex_create_info, memory_resource);
                 }
 
                 const auto index_create_info = BufferCreateInfo{index_buffer_size, BufferUsage::IndexBuffer};
                 for(auto& index_buffer : index_buffers) {
-                    index_buffer = render_engine.create_buffer(index_create_info, memory_resource);
+                    index_buffer = device.create_buffer(index_create_info, memory_resource);
                 }
 
                 return ntl::Result<bool>(true);
             })
             .on_error([](const ntl::NovaError& error) {
                 NOVA_LOG(ERROR) << "Could not allocate device memory for procedural mesh. Error: " << error.to_string();
+                // TODO: Propagate the error
             });
 
-        render_engine.allocate_device_memory(host_memory_size.b_count(), MemoryUsage::StagingBuffer, ObjectType::Buffer)
+        device.allocate_device_memory(host_memory_size.b_count(), MemoryUsage::StagingBuffer, ObjectType::Buffer)
             .map([&](DeviceMemory* memory) {
                 // TODO: Find a good way to keep these around
                 const auto
@@ -50,13 +57,114 @@ namespace nova::renderer {
                                                                                                        host_memory_size);
                 auto memory_resource = DeviceMemoryResource(memory, allocation_strategy.get());
 
-                cached_vertex_buffer = render_engine.create_buffer({vertex_buffer_size, BufferUsage::StagingBuffer}, memory_resource);
-                cached_index_buffer = render_engine.create_buffer({index_buffer_size, BufferUsage::StagingBuffer}, memory_resource);
+                cached_vertex_buffer = device.create_buffer({vertex_buffer_size, BufferUsage::StagingBuffer}, memory_resource);
+                cached_index_buffer = device.create_buffer({index_buffer_size, BufferUsage::StagingBuffer}, memory_resource);
 
                 return ntl::Result<bool>(true);
             })
             .on_error([](const ntl::NovaError& error) {
                 NOVA_LOG(ERROR) << "Could not allocate host memory for procedural mesh. Error: " << error.to_string();
+                // TODO: Propagate the error
             });
+    }
+
+    void ProceduralMesh::set_vertex_data(const void* data, const uint64_t size) {
+        render_engine.write_data_to_buffer(data, size, 0, cached_vertex_buffer);
+        num_vertex_bytes_to_upload = size;
+    }
+
+    void ProceduralMesh::set_index_data(const void* data, const uint64_t size) {
+        render_engine.write_data_to_buffer(data, size, 0, cached_index_buffer);
+        num_index_bytes_to_upload = size;
+    }
+
+    void ProceduralMesh::record_commands_to_upload_data(CommandList* cmds, const uint8_t frame_idx) {
+        const bool should_upload_vertex_buffer = num_vertex_bytes_to_upload > 0;
+        const bool should_upload_index_buffer = num_index_bytes_to_upload > 0;
+
+		auto* cur_vertex_buffer = vertex_buffers.at(frame_idx);
+		auto* cur_index_buffer = index_buffers.at(frame_idx);
+
+        std::vector<ResourceBarrier> barriers_before_upload;
+
+        if(should_upload_vertex_buffer) {
+            ResourceBarrier barrier_before_vertex_upload = {};
+            barrier_before_vertex_upload.resource_to_barrier = cur_vertex_buffer;
+            barrier_before_vertex_upload.access_before_barrier = AccessFlags::VertexAttributeRead;
+            barrier_before_vertex_upload.access_after_barrier = AccessFlags::MemoryWrite;
+            barrier_before_vertex_upload.old_state = ResourceState::VertexBuffer;
+            barrier_before_vertex_upload.new_state = ResourceState::CopyDestination;
+            barrier_before_vertex_upload.source_queue = QueueType::Graphics;
+            barrier_before_vertex_upload.destination_queue = QueueType::Transfer;
+            barrier_before_vertex_upload.buffer_memory_barrier.offset = 0;
+            barrier_before_vertex_upload.buffer_memory_barrier.size = num_vertex_bytes_to_upload;
+
+            barriers_before_upload.push_back(barrier_before_vertex_upload);
+        }
+
+        if(should_upload_index_buffer) {
+            ResourceBarrier barrier_before_index_upload = {};
+			barrier_before_index_upload.resource_to_barrier = cur_index_buffer;
+			barrier_before_index_upload.access_before_barrier = AccessFlags::IndexRead;
+			barrier_before_index_upload.access_after_barrier = AccessFlags::MemoryWrite;
+			barrier_before_index_upload.old_state = ResourceState::IndexBuffer;
+			barrier_before_index_upload.new_state = ResourceState::CopyDestination;
+			barrier_before_index_upload.source_queue = QueueType::Graphics;
+			barrier_before_index_upload.destination_queue = QueueType::Transfer;
+			barrier_before_index_upload.buffer_memory_barrier.offset = 0;
+			barrier_before_index_upload.buffer_memory_barrier.size = num_vertex_bytes_to_upload;
+
+			barriers_before_upload.push_back(barrier_before_index_upload);
+        }
+
+        if(barriers_before_upload.empty()) {
+            return;
+        }
+
+        cmds->resource_barriers(PipelineStageFlags::BottomOfPipe, PipelineStageFlags::Transfer, barriers_before_upload);
+
+        if(should_upload_vertex_buffer) {
+            cmds->copy_buffer(cur_vertex_buffer, 0, cached_vertex_buffer, 0, num_vertex_bytes_to_upload);
+        }
+
+        if(should_upload_index_buffer) {
+			cmds->copy_buffer(cur_index_buffer, 0, cached_index_buffer, 0, num_index_bytes_to_upload);
+        }
+
+        std::vector<ResourceBarrier> barriers_after_upload;
+
+        if(should_upload_vertex_buffer) {
+            ResourceBarrier barrier_after_vertex_upload = {};
+            barrier_after_vertex_upload.resource_to_barrier = cur_vertex_buffer;
+            barrier_after_vertex_upload.access_before_barrier = AccessFlags::MemoryWrite;
+            barrier_after_vertex_upload.access_after_barrier = AccessFlags::VertexAttributeRead;
+            barrier_after_vertex_upload.old_state = ResourceState::CopyDestination;
+            barrier_after_vertex_upload.new_state = ResourceState::VertexBuffer;
+            barrier_after_vertex_upload.source_queue = QueueType::Graphics;
+            barrier_after_vertex_upload.destination_queue = QueueType::Graphics;
+            barrier_after_vertex_upload.buffer_memory_barrier.offset = 0;
+            barrier_after_vertex_upload.buffer_memory_barrier.size = num_vertex_bytes_to_upload;
+            barriers_after_upload.push_back(barrier_after_vertex_upload);
+        }
+
+		if (should_upload_index_buffer) {
+			ResourceBarrier barrier_after_index_upload = {};
+			barrier_after_index_upload.resource_to_barrier = cur_index_buffer;
+			barrier_after_index_upload.access_before_barrier = AccessFlags::MemoryWrite;
+			barrier_after_index_upload.access_after_barrier = AccessFlags::IndexRead;
+			barrier_after_index_upload.old_state = ResourceState::CopyDestination;
+			barrier_after_index_upload.new_state = ResourceState::IndexBuffer;
+			barrier_after_index_upload.source_queue = QueueType::Graphics;
+			barrier_after_index_upload.destination_queue = QueueType::Graphics;
+			barrier_after_index_upload.buffer_memory_barrier.offset = 0;
+			barrier_after_index_upload.buffer_memory_barrier.size = num_index_bytes_to_upload;
+			barriers_after_upload.push_back(barrier_after_index_upload);
+		}
+
+		if (barriers_after_upload.empty()) {
+			return;
+		}
+
+        cmds->resource_barriers(PipelineStageFlags::Transfer, PipelineStageFlags::TopOfPipe, barriers_after_upload);
     }
 } // namespace nova::renderer
