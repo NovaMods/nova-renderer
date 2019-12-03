201ef049cfc832a6dd1490458b27ef1a5f02900f
David Dubois <dexcelstraun7@gmail.com>
2019-12-02 21:42:19 -0800

    [util] constexpr my butt

--- include/nova_renderer/constants.hpp
+++ include/nova_renderer/constants.hpp
@@ -5,12 +5,12 @@
 #include <string>
 
 namespace nova::renderer {
-    const std::string MODEL_MATRIX_BUFFER_NAME = "NovaModelMatrixUBO";
-    const std::string PER_FRAME_DATA_NAME = "NovaPerFrameUBO";
+    constexpr std::string_view MODEL_MATRIX_BUFFER_NAME = "NovaModelMatrixUBO";
+    constexpr std::string_view PER_FRAME_DATA_NAME = "NovaPerFrameUBO";
 
-    const uint32_t AMD_PCI_VENDOR_ID = 0x1022;
-    const uint32_t INTEL_PCI_VENDOR_ID = 8086;
-    const uint32_t NVIDIA_PCI_VENDOR_ID = 0x10DE;
+    constexpr uint32_t AMD_PCI_VENDOR_ID = 0x1022;
+    constexpr uint32_t INTEL_PCI_VENDOR_ID = 8086;
+    constexpr uint32_t NVIDIA_PCI_VENDOR_ID = 0x10DE;
 
-    const uint8_t NUM_IN_FLIGHT_FRAMES = 3;
+    constexpr uint8_t NUM_IN_FLIGHT_FRAMES = 3;
 }
