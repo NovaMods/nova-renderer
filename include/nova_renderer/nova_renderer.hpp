20680e9bf1f40f2ad4b75f0c4f46e1d8e1d2a100
David Dubois <dexcelstraun7@gmail.com>
2019-12-22 02:32:22 -0800

    [renderer] Fully implement making textures

--- include/nova_renderer/nova_renderer.hpp
+++ include/nova_renderer/nova_renderer.hpp
@@ -54,6 +54,7 @@ namespace nova::renderer {
 
         NovaRenderer(NovaRenderer&& other) noexcept = delete;
         NovaRenderer& operator=(NovaRenderer&& other) noexcept = delete;
+
         NovaRenderer(const NovaRenderer& other) = delete;
         NovaRenderer& operator=(const NovaRenderer& other) = delete;
 
@@ -97,6 +98,8 @@ namespace nova::renderer {
         void execute_frame();
 
         NovaSettingsAccessManager& get_settings();
+
+        std::shared_ptr<bvestl::polyalloc::allocator_handle> get_global_allocator();
 
 #pragma region Meshes
         /*!
