dfb421a1e8a44a62401cb5165b7b5fad7723666c
David Dubois <dexcelstraun7@gmail.com>
2020-01-05 14:49:15 -0800

    [filesystem] More better resource roots

--- include/nova_renderer/filesystem/virtual_filesystem.hpp
+++ include/nova_renderer/filesystem/virtual_filesystem.hpp
@@ -3,6 +3,7 @@
 #include <vector>
 
 #include "nova_renderer/util/filesystem.hpp"
+
 #include "folder_accessor.hpp"
 
 namespace nova::filesystem {
@@ -16,16 +17,35 @@ namespace nova::filesystem {
      */
     class VirtualFilesystem {
         friend class std::shared_ptr<VirtualFilesystem>;
+
     public:
         [[nodiscard]] static std::shared_ptr<VirtualFilesystem> get_instance();
+
+        /*!
+         * \brief Adds the provided path to the resource roots that the virtual filesystem will care about
+         */
+        void add_resource_root(const fs::path& root);
+
+        /*!
+         * \brief Adds the provided folder accessor as an accessor for one of our root directories
+         *
+         * This method lets you add a custom folder accessor as a resource root. This allows for e.g. the Minecraft adapter to register a
+         * shaderpack accessor which transpiles the shaders from GLSL 120 to SPIR-V
+         */
+        void add_resource_root(std::shared_ptr<FolderAccessorBase> root_accessor);
 
         [[nodiscard]] std::shared_ptr<FolderAccessorBase> get_folder_accessor(const fs::path& path) const;
 
     private:
         static std::shared_ptr<VirtualFilesystem> instance;
 
+        /*!
+         * \brief Accessor for Nova's working directory
+         */
+        std::shared_ptr<FolderAccessorBase> filesystem_root;
+
         std::vector<std::shared_ptr<FolderAccessorBase>> resource_roots;
 
         VirtualFilesystem();
     };
-} // namespace nova::fs
+} // namespace nova::filesystem
--- src/filesystem/virtual_filesystem.cpp
+++ src/filesystem/virtual_filesystem.cpp
@@ -1,5 +1,9 @@
 #include "nova_renderer/filesystem/virtual_filesystem.hpp"
 
+#include "nova_renderer/util/logger.hpp"
+
+#include "regular_folder_accessor.hpp"
+
 namespace nova::filesystem {
     std::shared_ptr<VirtualFilesystem> VirtualFilesystem::get_instance() {
         if(!instance) {
@@ -9,11 +13,24 @@ namespace nova::filesystem {
         return instance;
     }
 
+    void VirtualFilesystem::add_resource_root(const fs::path& root) {
+        resource_roots.emplace_back(filesystem_root->create_subfolder_accessor(root));
+    }
+
+    void VirtualFilesystem::add_resource_root(std::shared_ptr<FolderAccessorBase> root_accessor) {
+        resource_roots.emplace_back(root_accessor);
+    }
+
     std::shared_ptr<FolderAccessorBase> VirtualFilesystem::get_folder_accessor(const fs::path& path) const {
         for(const auto& root : resource_roots) {
             if(root->does_resource_exist(path)) {
                 return root->create_subfolder_accessor(path);
             }
         }
+
+        NOVA_LOG(ERROR) << "Could not file folder " << path.string();
+        return {};
     }
-} // namespace nova::fs
+
+    VirtualFilesystem::VirtualFilesystem() : filesystem_root(std::make_shared<RegularFolderAccessor>("./")) {}
+} // namespace nova::filesystem
