12349b9b86dd0eaa5bc37315d9ae5c3c0563e4a6
David Dubois <dexcelstraun7@gmail.com>
2020-01-07 01:05:24 -0800

    [pipelines] Actually save the vertex field information

--- src/renderer/pipeline_storage.cpp
+++ src/renderer/pipeline_storage.cpp
@@ -102,9 +102,11 @@ namespace nova::renderer {
             get_shader_module_descriptors(pipeline_create_info.fragment_shader->source, rhi::ShaderStage::Fragment, bindings);
         }
 
-        const auto vertex_attributes = get_vertex_fields(pipeline_create_info.vertex_shader);
-
-        return device->create_pipeline_interface(bindings, color_attachments, depth_texture, allocator);
+        return device->create_pipeline_interface(bindings, color_attachments, depth_texture, allocator)
+            .map([&](rhi::PipelineInterface* pipeline_interface) {
+                pipeline_interface->vertex_fields = get_vertex_fields(pipeline_create_info.vertex_shader);
+                return pipeline_interface;
+            });
     }
 
     rhi::VertexFieldFormat to_rhi_vertex_format(const SPIRType& spirv_type) {
