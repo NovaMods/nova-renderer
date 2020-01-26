92f8f2f5603cdb6e9d2fb09e8bbcb026724d03de
David Dubois <dexcelstraun7@gmail.com>
2020-01-26 13:54:46 -0800

    [filesystem] Un-bork some files

--- src/filesystem/regular_folder_accessor.hpp
+++ src/filesystem/regular_folder_accessor.hpp
@@ -9,17 +9,16 @@ namespace nova::filesystem {
     class RegularFolderAccessor final : public FolderAccessorBase {
     public:
         explicit RegularFolderAccessor(const rx::string& folder);
-    
+
         ~RegularFolderAccessor() override = default;
 
-        std::string read_text_file(const fs::path& resource_path) override;
+        rx::vector<uint8_t> read_file(const rx::string& path) override;
 
-        std::pmr::vector<fs::path> get_all_items_in_folder(const fs::path& folder) override;
+        rx::vector<rx::string> get_all_items_in_folder(const rx::string& folder) override;
 
-        std::shared_ptr<FolderAccessorBase> create_subfolder_accessor(const fs::path& path) const override;
+        bool does_resource_exist_on_filesystem(const rx::string& resource_path) override;
 
     protected:
-        bool does_resource_exist_on_filesystem(const fs::path& resource_path) override;
-    
+        FolderAccessorBase* create_subfolder_accessor(const rx::string& path) const override;
     };
-} // namespace nova::renderer
+} // namespace nova::filesystem
