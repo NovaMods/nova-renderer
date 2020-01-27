59298dfa5827f34db4b96685ffa833ed9278481f
David Dubois <dexcelstraun7@gmail.com>
2020-01-26 17:48:49 -0800

    [rhi] Swapchain uses Rex types

--- src/rhi/swapchain.cpp
+++ src/rhi/swapchain.cpp
@@ -3,11 +3,11 @@
 namespace nova::renderer::rhi {
     Swapchain::Swapchain(const uint32_t num_images, const glm::uvec2& size) : num_images(num_images), size(size) {}
 
-    Framebuffer* Swapchain::get_framebuffer(const uint32_t frame_idx) const { return framebuffers.at(frame_idx); }
+    Framebuffer* Swapchain::get_framebuffer(const uint32_t frame_idx) const { return framebuffers[frame_idx]; }
 
-    Image* Swapchain::get_image(const uint32_t frame_idx) const { return swapchain_images.at(frame_idx); }
+    Image* Swapchain::get_image(const uint32_t frame_idx) const { return swapchain_images[frame_idx]; }
 
-    Fence* Swapchain::get_fence(const uint32_t frame_idx) const { return fences.at(frame_idx); }
+    Fence* Swapchain::get_fence(const uint32_t frame_idx) const { return fences[frame_idx]; }
 
     glm::uvec2 Swapchain::get_size() const { return size; }
 } // namespace nova::renderer::rhi
