eb72c4bf53ef0c6a1cb126e6fada0a4a4a67381c
David Dubois <dexcelstraun7@gmail.com>
2020-01-30 21:01:56 -0800

    [validation] Output JSON of default values

--- src/loading/shaderpack/shaderpack_validator.cpp
+++ src/loading/shaderpack/shaderpack_validator.cpp
@@ -257,8 +257,10 @@ namespace nova::renderer::shaderpack {
         rx::json& j, const char* field_name, const rx::string& context, const rx::json& default_value, ValidationReport& report) {
         if(!j[field_name]) {
             j[field_name] = default_value[field_name];
+            size_t out_size;
+            const char* json_string = reinterpret_cast<const char*>(json_write_minified(j[field_name].raw(), &out_size));
             report.warnings.emplace_back(context + ": Missing field " + field_name + ". A default value of '" +
-                                         json_write_minified(j[field_name]) + "' will be used");
+                                         json_string + "' will be used");
         }
     }
 
