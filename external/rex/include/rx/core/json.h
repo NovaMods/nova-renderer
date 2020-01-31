d6e2a81b93d90a158ac2e9c16de7498196b3b101
David Dubois <dexcelstraun7@gmail.com>
2020-01-30 21:01:08 -0800

    [external::rex] Add method to get the raw JSON value

--- external/rex/include/rx/core/json.h
+++ external/rex/include/rx/core/json.h
@@ -1,241 +1,188 @@
 #ifndef RX_CORE_JSON_H
 #define RX_CORE_JSON_H
-#include "rx/core/concurrency/atomic.h"
-
-#include "rx/core/traits/return_type.h"
-#include "rx/core/traits/is_same.h"
-
-#include "rx/core/string.h"
-#include "rx/core/optional.h"
-
 #include "lib/json.h"
+#include "rx/core/concurrency/atomic.h"
+#include "rx/core/optional.h"
+#include "rx/core/string.h"
+#include "rx/core/traits/is_same.h"
+#include "rx/core/traits/return_type.h"
 
 namespace rx {
 
-struct json {
-  constexpr json();
-  json(memory::allocator* _allocator, const char* _contents, rx_size _length);
-  json(memory::allocator* _allocator, const char* _contents);
-  json(memory::allocator* _allocator, const string& _contents);
-  json(const char* _contents, rx_size _length);
-  json(const char* _contents);
-  json(const string& _contents);
-  json(const json& _json);
-  json(json&& json_);
-  ~json();
+    struct json {
+        constexpr json();
+        json(memory::allocator* _allocator, const char* _contents, rx_size _length);
+        json(memory::allocator* _allocator, const char* _contents);
+        json(memory::allocator* _allocator, const string& _contents);
+        json(const char* _contents, rx_size _length);
+        json(const char* _contents);
+        json(const string& _contents);
+        json(const json& _json);
+        json(json&& json_);
+        ~json();
 
-  json& operator=(const json& _json);
-  json& operator=(json&& json_);
+        json& operator=(const json& _json);
+        json& operator=(json&& json_);
 
-  enum class type {
-    k_array,
-    k_boolean,
-    k_null,
-    k_number,
-    k_object,
-    k_string,
-    k_integer
-  };
+        enum class type { k_array, k_boolean, k_null, k_number, k_object, k_string, k_integer };
 
-  operator bool() const;
-  optional<string> error() const;
+        operator bool() const;
+        optional<string> error() const;
 
-  bool is_type(type _type) const;
+        bool is_type(type _type) const;
 
-  bool is_array() const;
-  bool is_array_of(type _type) const;
-  bool is_array_of(type _type, rx_size _size) const;
-  bool is_boolean() const;
-  bool is_null() const;
-  bool is_number() const;
-  bool is_object() const;
-  bool is_string() const;
-  bool is_integer() const;
+        bool is_array() const;
+        bool is_array_of(type _type) const;
+        bool is_array_of(type _type, rx_size _size) const;
+        bool is_boolean() const;
+        bool is_null() const;
+        bool is_number() const;
+        bool is_object() const;
+        bool is_string() const;
+        bool is_integer() const;
 
-  json operator[](rx_size _index) const;
-  bool as_boolean() const;
-  rx_f64 as_number() const;
-  rx_f32 as_float() const;
-  rx_s32 as_integer() const;
-  json operator[](const char* _name) const;
-  string as_string() const;
-  string as_string_with_allocator(memory::allocator* _allocator) const;
+        json operator[](rx_size _index) const;
+        bool as_boolean() const;
+        rx_f64 as_number() const;
+        rx_f32 as_float() const;
+        rx_s32 as_integer() const;
+        json operator[](const char* _name) const;
+        string as_string() const;
+        string as_string_with_allocator(memory::allocator* _allocator) const;
 
-  // # of elements for objects and arrays only
-  rx_size size() const;
-  bool is_empty() const;
+        // # of elements for objects and arrays only
+        rx_size size() const;
+        bool is_empty() const;
 
-  template<typename F>
-  bool each(F&& _function) const;
+        template <typename F>
+        bool each(F&& _function) const;
 
-  memory::allocator* allocator() const;
+        memory::allocator* allocator() const;
 
-private:
-  struct shared {
-    shared(memory::allocator* _allocator, const char* _contents, rx_size _length);
-    ~shared();
+        struct json_value_s* raw() const;
 
-    shared* acquire();
-    void release();
+    private:
+        struct shared {
+            shared(memory::allocator* _allocator, const char* _contents, rx_size _length);
+            ~shared();
 
-    memory::allocator* m_allocator;
-    struct json_parse_result_s m_error;
-    struct json_value_s* m_root;
-    concurrency::atomic<rx_size> m_count;
-  };
+            shared* acquire();
+            void release();
 
-  json(shared* _shared, struct json_value_s* _head);
+            memory::allocator* m_allocator;
+            struct json_parse_result_s m_error;
+            struct json_value_s* m_root;
+            concurrency::atomic<rx_size> m_count;
+        };
 
-  shared* m_shared;
-  struct json_value_s* m_value;
-};
+        json(shared* _shared, struct json_value_s* _head);
 
-inline constexpr json::json()
-  : m_shared{nullptr}
-  , m_value{nullptr}
-{
-}
+        shared* m_shared;
+        struct json_value_s* m_value;
+    };
 
-inline json::json(memory::allocator* _allocator, const string& _contents)
-  : json{_allocator, _contents.data(), _contents.size()}
-{
-}
+    inline constexpr json::json() : m_shared{nullptr}, m_value{nullptr} {}
 
-inline json::json(const char* _contents, rx_size _length)
-  : json{&memory::g_system_allocator, _contents, _length}
-{
-}
+    inline json::json(memory::allocator* _allocator, const string& _contents) : json{_allocator, _contents.data(), _contents.size()} {}
 
-inline json::json(const string& _contents)
-  : json{&memory::g_system_allocator, _contents.data(), _contents.size()}
-{
-}
+    inline json::json(const char* _contents, rx_size _length) : json{&memory::g_system_allocator, _contents, _length} {}
 
-inline json::json(const json& _json)
-  : m_shared{_json.m_shared->acquire()}
-  , m_value{_json.m_value}
-{
-}
+    inline json::json(const string& _contents) : json{&memory::g_system_allocator, _contents.data(), _contents.size()} {}
 
-inline json::json(json&& json_)
-  : m_shared{json_.m_shared}
-  , m_value{json_.m_value}
-{
-  json_.m_shared = nullptr;
-  json_.m_value = nullptr;
-}
+    inline json::json(const json& _json) : m_shared{_json.m_shared->acquire()}, m_value{_json.m_value} {}
 
-inline json::~json() {
-  if (m_shared) {
-    m_shared->release();
-  }
-}
-
-inline json& json::operator=(const json& _json) {
-  RX_ASSERT(&_json != this, "self assignment");
-
-  if (m_shared) {
-    m_shared->release();
-  }
-
-  m_shared = _json.m_shared->acquire();
-  m_value = _json.m_value;
-
-  return *this;
-}
-
-inline json& json::operator=(json&& json_) {
-  RX_ASSERT(&json_ != this, "self assignment");
-
-  m_shared = json_.m_shared;
-  m_value = json_.m_value;
-  json_.m_shared = nullptr;
-  json_.m_value = nullptr;
-
-  return *this;
-}
-
-inline json::operator bool() const {
-  return m_shared && m_shared->m_root;
-}
-
-inline bool json::is_array() const {
-  return is_type(type::k_array);
-}
-
-inline bool json::is_array_of(type _type) const {
-  if (!is_array()) {
-    return false;
-  }
-
-  return each([_type](const json& _value) {
-    return _value.is_type(_type);
-  });
-}
-
-inline bool json::is_array_of(type _type, rx_size _size) const {
-  if (!is_array()) {
-    return false;
-  }
-
-  if (size() != _size) {
-    return false;
-  }
-
-  return each([_type](const json& _value) {
-    return _value.is_type(_type);
-  });
-}
-
-inline bool json::is_boolean() const {
-  return is_type(type::k_boolean);
-}
-
-inline bool json::is_null() const {
-  return is_type(type::k_null);
-}
-
-inline bool json::is_number() const {
-  return is_type(type::k_number);
-}
-
-inline bool json::is_object() const {
-  return is_type(type::k_object);
-}
-
-inline bool json::is_string() const {
-  return is_type(type::k_string);
-}
-
-inline bool json::is_integer() const {
-  return is_type(type::k_integer);
-}
-
-inline bool json::is_empty() const {
-  return size() != 0;
-}
-
-inline string json::as_string() const {
-  return as_string_with_allocator(&memory::g_system_allocator);
-}
-
-template<typename F>
-inline bool json::each(F&& _function) const {
-  for (rx_size i{0}; i < size(); i++) {
-    if constexpr(traits::is_same<traits::return_type<F>, bool>) {
-      if (!_function(operator[](i))) {
-        return false;
-      }
-    } else {
-      _function(operator[](i));
+    inline json::json(json&& json_) : m_shared{json_.m_shared}, m_value{json_.m_value} {
+        json_.m_shared = nullptr;
+        json_.m_value = nullptr;
     }
-  }
-  return true;
-}
 
-inline memory::allocator* json::allocator() const {
-  return m_shared ? m_shared->m_allocator : nullptr;
-}
+    inline json::~json() {
+        if(m_shared) {
+            m_shared->release();
+        }
+    }
+
+    inline json& json::operator=(const json& _json) {
+        RX_ASSERT(&_json != this, "self assignment");
+
+        if(m_shared) {
+            m_shared->release();
+        }
+
+        m_shared = _json.m_shared->acquire();
+        m_value = _json.m_value;
+
+        return *this;
+    }
+
+    inline json& json::operator=(json&& json_) {
+        RX_ASSERT(&json_ != this, "self assignment");
+
+        m_shared = json_.m_shared;
+        m_value = json_.m_value;
+        json_.m_shared = nullptr;
+        json_.m_value = nullptr;
+
+        return *this;
+    }
+
+    inline json::operator bool() const { return m_shared && m_shared->m_root; }
+
+    inline bool json::is_array() const { return is_type(type::k_array); }
+
+    inline bool json::is_array_of(type _type) const {
+        if(!is_array()) {
+            return false;
+        }
+
+        return each([_type](const json& _value) { return _value.is_type(_type); });
+    }
+
+    inline bool json::is_array_of(type _type, rx_size _size) const {
+        if(!is_array()) {
+            return false;
+        }
+
+        if(size() != _size) {
+            return false;
+        }
+
+        return each([_type](const json& _value) { return _value.is_type(_type); });
+    }
+
+    inline bool json::is_boolean() const { return is_type(type::k_boolean); }
+
+    inline bool json::is_null() const { return is_type(type::k_null); }
+
+    inline bool json::is_number() const { return is_type(type::k_number); }
+
+    inline bool json::is_object() const { return is_type(type::k_object); }
+
+    inline bool json::is_string() const { return is_type(type::k_string); }
+
+    inline bool json::is_integer() const { return is_type(type::k_integer); }
+
+    inline bool json::is_empty() const { return size() != 0; }
+
+    inline string json::as_string() const { return as_string_with_allocator(&memory::g_system_allocator); }
+
+    template <typename F>
+    inline bool json::each(F&& _function) const {
+        for(rx_size i{0}; i < size(); i++) {
+            if constexpr(traits::is_same<traits::return_type<F>, bool>) {
+                if(!_function(operator[](i))) {
+                    return false;
+                }
+            } else {
+                _function(operator[](i));
+            }
+        }
+        return true;
+    }
+
+    inline memory::allocator* json::allocator() const { return m_shared ? m_shared->m_allocator : nullptr; }
+
+    inline struct json_value_s* json::raw() const { return m_value; }
 
 } // namespace rx
 
