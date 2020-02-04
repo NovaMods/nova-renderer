f9f86d8979a248e183d1364c5bced2918c9d4209
Janrupf <werbung.janrupf@t-online.de>
2020-02-04 18:25:21 +0100

    [linux] Fix SIGSEGV in linux_utils.cpp

--- src/util/linux_utils.cpp
+++ src/util/linux_utils.cpp
@@ -1,65 +1,51 @@
-#include "linux_utils.hpp"
-
-#include <exception>
+#include <array>
+#include <cstdlib>
+#include <iostream>
+#include <string>
 
 #include <cxxabi.h>
-#include <errno.h>
 #include <execinfo.h>
-#include <rx/core/array.h>
-#include <rx/core/log.h>
-#include <stdlib.h>
-#include <string.h>
 
 void nova_backtrace() {
-    RX_LOG("LinuxUtil", logger);
-
-    rx::array<void* [50]> array {};
+    std::array<void*, 50> array{};
 
     // get void*'s for all entries on the stack
-    int size = backtrace(array.data(), 10);
+    int size = backtrace(array.data(), array.size());
 
     // print out all the frames to stderr
-    logger(rx::log::level::k_error, "Stacktrace: ");
+    std::cerr << "Stacktrace: " << std::endl;
     char** data = backtrace_symbols(array.data(), size);
 
     for(int i = 0; i < size; i++) {
-        rx::string str(data[i]);
+        std::string str(data[i]);
 
-        if(str.find_last_of("(") != rx::string::k_npos && str.find_last_of(")") != rx::string::k_npos) {
+        if(str.find_last_of('(') != std::string::npos && str.find_last_of(')') != std::string::npos) {
             try {
-                rx::string path = str.substring(0, str.find_last_of("("));
-                rx::string symbol = str.substring(str.find_last_of("(") + 1, str.find_last_of("+") - str.find_last_of("(") - 1);
-                rx::string address = str.substring(str.find_last_of("+"), str.find_last_of(")") - str.find_last_of("+"));
+                std::string path = str.substr(0, str.find_last_of('('));
+                std::string symbol = str.substr(str.find_last_of('(') + 1, str.find_last_of('+') - str.find_last_of('(') - 1);
+                std::string address = str.substr(str.find_last_of('+'), str.find_last_of(')') - str.find_last_of('+'));
+                std::string load_address = str.substr(str.find_last_of('[') + 1, str.find_last_of(']') - str.find_last_of('['));
 
-                if(symbol.size() > 0) {
+                if(!symbol.empty()) {
                     char* name = abi::__cxa_demangle(symbol.data(), nullptr, nullptr, nullptr);
-                    str = rx::string::format("%s(%s%s)", path, name, address);
+
+                    str = std::string(path) + "(" + std::string(name ? name : symbol.c_str()) + std::string(address) + ") [" + load_address + "]";
 
                     // NOLINTNEXTLINE(cppcoreguidelines-no-malloc,cppcoreguidelines-owning-memory)
                     free(name);
                 }
             }
             catch(const std::exception& e) {
-                logger(rx::log::level::k_warning, "Demangle failed: %s", e.what());
+                std::cerr << "Demangle failed: " << e.what() << std::endl;
             }
         }
 
         if(str[str.size() - 1] == '\n') {
-            str = str.substring(0, str.size() - 1);
+            str = str.substr(0, str.size() - 1);
         }
 
-        logger(rx::log::level::k_error, "\t%s", str);
+        std::cerr << "\t" << str << std::endl;
     }
     // NOLINTNEXTLINE(cppcoreguidelines-no-malloc,cppcoreguidelines-owning-memory)
     free(data);
-}
-
-rx::string get_last_linux_error() {
-    char* errstr = strerror(errno);
-    if(errstr == nullptr) {
-        return rx::string("unkown error");
-    }
-    errno = 0;
-
-    return rx::string(errstr);
 }
