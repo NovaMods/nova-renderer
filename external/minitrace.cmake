3cc3f5d46f5b525ed501cbef6b88fffa4a8ab501
David Dubois <dexcelstraun7@gmail.com>
2020-02-12 08:54:11 -0800

    [external::minitrace] Enable minitrace properly

--- external/minitrace.cmake
+++ external/minitrace.cmake
@@ -5,3 +5,4 @@ add_library(minitrace STATIC ${MINITRACE_SOURCE})
 target_include_directories(minitrace SYSTEM PUBLIC
         $<BUILD_INTERFACE:${CMAKE_CURRENT_LIST_DIR}/minitrace>
         $<INSTALL_INTERFACE:minitrace>)
+target_compile_definitions(minitrace PUBLIC MTR_ENABLED)
