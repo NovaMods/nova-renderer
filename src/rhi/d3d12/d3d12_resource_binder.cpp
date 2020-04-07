0ea84ee2d18883399162dd83c53f1661a29ad65e
David Dubois <dexcelstraun7@gmail.com>
2020-04-06 23:34:54 -0700

    [rehi::d3d12] _finally_ completed the abstraction for binding resources

--- src/rhi/d3d12/d3d12_resource_binder.cpp
+++ src/rhi/d3d12/d3d12_resource_binder.cpp
@@ -9,60 +9,63 @@ namespace nova::renderer::rhi {
     RX_LOG("D3D12ResourceBinder", logger);
 
     D3D12ResourceBinder::D3D12ResourceBinder(rx::memory::allocator& allocator,
+                                             INT descriptor_size_in,
                                              Microsoft::WRL::ComPtr<ID3D12Device> device_in,
                                              Microsoft::WRL::ComPtr<ID3D12RootSignature> root_signature_in,
                                              rx::vector<D3D12RootParameter> root_parameters_in,
                                              rx::map<rx::string, D3D12Descriptor> descriptor_table_bindings_in)
-        : bound_images{allocator},
+        : internal_allocator{&allocator},
+          bound_images{allocator},
           bound_buffers{allocator},
           bound_samplers{allocator},
           device{rx::utility::move(device_in)},
           root_signature{rx::utility::move(root_signature_in)},
           root_parameters{rx::utility::move(root_parameters_in)},
-          descriptor_table_bindings{rx::utility::move(descriptor_table_bindings_in)} {}
+          descriptor_table_bindings{rx::utility::move(descriptor_table_bindings_in)},
+          descriptor_size{descriptor_size_in} {}
 
     void D3D12ResourceBinder::bind_image(const rx::string& binding_name, RhiImage* image) {
-        auto* d3d12_image = static_cast<D3D12Image*>(image);
-        if(auto* image_slot = bound_images.find(binding_name)) {
-            *image_slot = d3d12_image;
-
-        } else {
-            bound_images.insert(binding_name, d3d12_image);
-        }
-
-        is_dirty = true;
+        bind_image_array(binding_name, rx::array{image});
     }
 
     void D3D12ResourceBinder::bind_buffer(const rx::string& binding_name, RhiBuffer* buffer) {
-        auto* d3d12_buffer = static_cast<D3D12Buffer*>(buffer);
+        bind_buffer_array(binding_name, rx::array{buffer});
+    }
+
+    void D3D12ResourceBinder::bind_sampler(const rx::string& /* binding_name */, RhiSampler* /* sampler */) {
+        logger->error("D3D12 doesn't support binding samplers");
+    }
+
+    void D3D12ResourceBinder::bind_image_array(const rx::string& binding_name, const rx::vector<RhiImage*>& images) {
+        rx::vector<D3D12Image*> d3d12_images{*internal_allocator};
+        d3d12_images.reserve(images.size());
+
+        images.each_fwd([&](RhiImage* image) { d3d12_images.push_back(static_cast<D3D12Image*>(image)); });
+
+        if(auto* image_slot = bound_images.find(binding_name)) {
+            *image_slot += d3d12_images;
+
+        } else {
+            bound_images.insert(binding_name, d3d12_images);
+        }
+
+        is_dirty = true;
+    }
+
+    void D3D12ResourceBinder::bind_buffer_array(const rx::string& binding_name, const rx::vector<RhiBuffer*>& buffers) {
+        rx::vector<D3D12Buffer*> d3d12_buffers{*internal_allocator};
+        d3d12_buffers.reserve(buffers.size());
+
+        buffers.each_fwd([&](RhiBuffer* image) { d3d12_buffers.push_back(static_cast<D3D12Buffer*>(image)); });
+
         if(auto* buffer_slot = bound_buffers.find(binding_name)) {
-            *buffer_slot = d3d12_buffer;
+            *buffer_slot += d3d12_buffers;
 
         } else {
-            bound_buffers.insert(binding_name, d3d12_buffer);
+            bound_buffers.insert(binding_name, d3d12_buffers);
         }
 
         is_dirty = true;
-    }
-
-    void D3D12ResourceBinder::bind_sampler(const rx::string& binding_name, RhiSampler* sampler) {
-        auto* d3d12_sampler = static_cast<D3D12Sampler*>(sampler);
-        if(auto* sampler_slot = bound_samplers.find(binding_name)) {
-            *sampler_slot = d3d12_sampler;
-
-        } else {
-            bound_samplers.insert(binding_name, d3d12_sampler);
-        }
-
-        is_dirty = true;
-    }
-
-    void D3D12ResourceBinder::bind_image_array(const rx::string& /* binding_name */, const rx::vector<RhiImage*>& /* images */) {
-        logger->error("D3D12 doesn't support binding image arrays");
-    }
-
-    void D3D12ResourceBinder::bind_buffer_array(const rx::string& /* binding_name */, const rx::vector<RhiBuffer*>& /* buffers */) {
-        logger->error("D3D12 doesn't support binding buffer arrays");
     }
 
     void D3D12ResourceBinder::bind_sampler_array(const rx::string& /* binding_name */, const rx::vector<RhiSampler*>& /* samplers */) {
@@ -73,8 +76,8 @@ namespace nova::renderer::rhi {
         MTR_SCOPE("D3D12ResourceBinder", "update_descriptors");
 
         descriptor_table_bindings.each_pair([&](const rx::string& binding, const D3D12Descriptor& descriptor) {
-            if(auto* image_slot = bound_images.find(binding)) {
-                auto* image = *image_slot;
+            if(auto* images_slot = bound_images.find(binding)) {
+                auto& images = *images_slot;
 
                 switch(descriptor.type) {
                     case D3D12ResourceType::CBV: {
@@ -82,62 +85,28 @@ namespace nova::renderer::rhi {
                     } break;
 
                     case D3D12ResourceType::SRV: {
-                        D3D12_SHADER_RESOURCE_VIEW_DESC srv_desc{};
-                        srv_desc.Format = image->format;
-                        srv_desc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
-                        srv_desc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
-                        srv_desc.Texture2D.MostDetailedMip = 0;
-                        srv_desc.Texture2D.MipLevels = -1;
-                        srv_desc.Texture2D.PlaneSlice = 0;
-                        srv_desc.Texture2D.ResourceMinLODClamp = 0;
-
-                        device->CreateShaderResourceView(image->resource.Get(), &srv_desc, descriptor.handle);
+                        create_srv_array(images, descriptor);
                     } break;
 
                     case D3D12ResourceType::UAV: {
-                        D3D12_UNORDERED_ACCESS_VIEW_DESC uav_desc{};
-                        uav_desc.Format = image->format;
-                        uav_desc.ViewDimension = D3D12_UAV_DIMENSION_TEXTURE2D;
-                        uav_desc.Texture2D.MipSlice = 0;
-                        uav_desc.Texture2D.PlaneSlice = 0;
-
-                        device->CreateUnorderedAccessView(image->resource.Get(), nullptr, &uav_desc, descriptor.handle);
+                        create_uav_array(images, descriptor);
                     } break;
                 }
 
-            } else if(auto* buffer_slot = bound_buffers.find(binding)) {
-                auto* buffer = *buffer_slot;
+            } else if(auto* buffers_slot = bound_buffers.find(binding)) {
+                auto* buffers = *buffers_slot;
 
                 switch(descriptor.type) {
                     case D3D12ResourceType::CBV: {
-                        D3D12_CONSTANT_BUFFER_VIEW_DESC cbv_desc{};
-                        cbv_desc.BufferLocation = buffer->resource->GetGPUVirtualAddress();
-                        cbv_desc.SizeInBytes = static_cast<UINT>(buffer->size.b_count());
-
-                        device->CreateConstantBufferView(&cbv_desc, descriptor.handle);
+                        create_cbv_array(buffers, descriptor);
                     } break;
 
                     case D3D12ResourceType::SRV: {
-                        D3D12_SHADER_RESOURCE_VIEW_DESC srv_desc{};
-                        srv_desc.Format = DXGI_FORMAT_UNKNOWN;
-                        srv_desc.ViewDimension = D3D12_SRV_DIMENSION_BUFFER;
-                        srv_desc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
-                        srv_desc.Buffer.FirstElement = 0;
-                        srv_desc.Buffer.NumElements = buffer->size.b_count() / descriptor.array_element_size;
-                        srv_desc.Buffer.StructureByteStride = descriptor.array_element_size;
-
-                        device->CreateShaderResourceView(buffer->resource.Get(), &srv_desc, descriptor.handle);
+                        create_srv_array(buffers, descriptor);
                     } break;
 
                     case D3D12ResourceType::UAV: {
-                        D3D12_UNORDERED_ACCESS_VIEW_DESC uav_desc{};
-                        uav_desc.Format = DXGI_FORMAT_UNKNOWN;
-                        uav_desc.ViewDimension = D3D12_UAV_DIMENSION_BUFFER;
-                        uav_desc.Buffer.FirstElement = 0;
-                        uav_desc.Buffer.NumElements = buffer->size.b_count() / descriptor.array_element_size;
-                        uav_desc.Buffer.StructureByteStride = descriptor.array_element_size;
-
-                        device->CreateUnorderedAccessView(buffer->resource.Get(), nullptr, &uav_desc, descriptor.handle);
+                        create_uav_array(buffers, descriptor);
                     } break;
                 }
 
@@ -167,7 +136,13 @@ namespace nova::renderer::rhi {
                                                    const D3D12RootDescriptor& root_descriptor,
                                                    ID3D12GraphicsCommandList* cmds) const {
         if(const auto* image_slot = bound_images.find(root_descriptor.binding_name)) {
-            const auto* image = *image_slot;
+            const auto& images = *image_slot;
+            if(images.size() != 1) {
+                logger->error("Can not bind more than one resource to root binding %s", root_descriptor.binding_name);
+                return;
+            }
+
+            const auto* image = images[0];
 
             switch(root_descriptor.type) {
                 case D3D12ResourceType::CBV: {
@@ -184,7 +159,13 @@ namespace nova::renderer::rhi {
             }
 
         } else if(const auto* buffer_slot = bound_buffers.find(root_descriptor.binding_name)) {
-            const auto* buffer = *buffer_slot;
+            const auto& buffers = *buffer_slot;
+            if(buffers.size() != 1) {
+                logger->error("Can not bind more than one resource to root binding %s", root_descriptor.binding_name);
+                return;
+            }
+
+            const auto* buffer = buffers[0];
 
             switch(root_descriptor.type) {
                 case D3D12ResourceType::CBV: {
@@ -203,5 +184,89 @@ namespace nova::renderer::rhi {
         } else {
             logger->error("No resources bound to binding %s", root_descriptor.binding_name);
         }
+    }
+
+    void D3D12ResourceBinder::create_srv_array(const rx::vector<D3D12Image*>& images, const D3D12Descriptor& start_descriptor) const {
+        CD3DX12_CPU_DESCRIPTOR_HANDLE handle{start_descriptor.handle};
+
+        images.each_fwd([&](const D3D12Image* image) {
+            D3D12_SHADER_RESOURCE_VIEW_DESC srv_desc{};
+            srv_desc.Format = image->format;
+            srv_desc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+            srv_desc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+            srv_desc.Texture2D.MostDetailedMip = 0;
+            srv_desc.Texture2D.MipLevels = -1;
+            srv_desc.Texture2D.PlaneSlice = 0;
+            srv_desc.Texture2D.ResourceMinLODClamp = 0;
+
+            device->CreateShaderResourceView(image->resource.Get(), &srv_desc, handle);
+
+            handle.Offset(descriptor_size);
+        });
+    }
+
+    void D3D12ResourceBinder::create_uav_array(const rx::vector<D3D12Image*>& images, const D3D12Descriptor& start_descriptor) const {
+        CD3DX12_CPU_DESCRIPTOR_HANDLE handle{start_descriptor.handle};
+
+        images.each_fwd([&](const D3D12Image* image) {
+            D3D12_UNORDERED_ACCESS_VIEW_DESC uav_desc;
+            uav_desc.Format = image->format;
+            uav_desc.ViewDimension = D3D12_UAV_DIMENSION_TEXTURE2D;
+            uav_desc.Texture2D.MipSlice = 0;
+            uav_desc.Texture2D.PlaneSlice = 0;
+
+            device->CreateUnorderedAccessView(image->resource.Get(), nullptr, &uav_desc, handle);
+
+            handle.Offset(descriptor_size);
+        });
+    }
+
+    void D3D12ResourceBinder::create_cbv_array(const rx::vector<D3D12Buffer*>& buffers, const D3D12Descriptor& start_descriptor) const {
+        CD3DX12_CPU_DESCRIPTOR_HANDLE handle{start_descriptor.handle};
+
+        buffers.each_fwd([&](const D3D12Buffer* buffer) {
+            D3D12_CONSTANT_BUFFER_VIEW_DESC cbv_desc{};
+            cbv_desc.BufferLocation = buffer->resource->GetGPUVirtualAddress();
+            cbv_desc.SizeInBytes = static_cast<UINT>(buffer->size.b_count());
+
+            device->CreateConstantBufferView(&cbv_desc, handle);
+
+            handle.Offset(descriptor_size);
+        });
+    }
+
+    void D3D12ResourceBinder::create_srv_array(const rx::vector<D3D12Buffer*>& buffers, const D3D12Descriptor& start_descriptor) const {
+        CD3DX12_CPU_DESCRIPTOR_HANDLE handle{start_descriptor.handle};
+
+        buffers.each_fwd([&](const D3D12Buffer* buffer) {
+            D3D12_SHADER_RESOURCE_VIEW_DESC srv_desc{};
+            srv_desc.Format = DXGI_FORMAT_UNKNOWN;
+            srv_desc.ViewDimension = D3D12_SRV_DIMENSION_BUFFER;
+            srv_desc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+            srv_desc.Buffer.FirstElement = 0;
+            srv_desc.Buffer.NumElements = buffer->size.b_count() / start_descriptor.array_element_size;
+            srv_desc.Buffer.StructureByteStride = start_descriptor.array_element_size;
+
+            device->CreateShaderResourceView(buffer->resource.Get(), &srv_desc, handle);
+
+            handle.Offset(descriptor_size);
+        });
+    }
+
+    void D3D12ResourceBinder::create_uav_array(const rx::vector<D3D12Buffer*>& buffers, const D3D12Descriptor& start_descriptor) const {
+        CD3DX12_CPU_DESCRIPTOR_HANDLE handle{start_descriptor.handle};
+
+        buffers.each_fwd([&](const D3D12Buffer* buffer) {
+            D3D12_UNORDERED_ACCESS_VIEW_DESC uav_desc{};
+            uav_desc.Format = DXGI_FORMAT_UNKNOWN;
+            uav_desc.ViewDimension = D3D12_UAV_DIMENSION_BUFFER;
+            uav_desc.Buffer.FirstElement = 0;
+            uav_desc.Buffer.NumElements = buffer->size.b_count() / start_descriptor.array_element_size;
+            uav_desc.Buffer.StructureByteStride = start_descriptor.array_element_size;
+
+            device->CreateUnorderedAccessView(buffer->resource.Get(), nullptr, &uav_desc, handle);
+
+            handle.Offset(descriptor_size);
+        });
     }
 } // namespace nova::renderer::rhi
--- src/rhi/d3d12/d3d12_resource_binder.hpp
+++ src/rhi/d3d12/d3d12_resource_binder.hpp
@@ -51,6 +51,12 @@ namespace nova::renderer::rhi {
 
     /*!
      * \brief Allows the user to bind resources to a D3D12 command list in a sane way
+     *
+     * This class makes a lot of assumptions that all the input data is correct
+     *
+     * Important note: descriptors for arrays in the shader MUST have enough free space in the descriptor heap to hold the full array. For
+     * things like Nova's textures array, this probably means that you need space for 128k descriptors? I don't have a solid idea of how
+     * many textures Nova will support at once, but it'll probably be a few hundred thousand at least
      */
     class D3D12ResourceBinder : public RhiResourceBinder {
     public:
@@ -58,6 +64,7 @@ namespace nova::renderer::rhi {
          * \brief Creates a new D3D12ResourceBinder
          *
          * \param allocator The allocator that this instance should use internally
+         * \param descriptor_size_in The size of one descriptor
          * \param device_in The ID3D12Device which will consume these resource bindings
          * \param root_signature_in The root signature that this resource binder wil bind resources to
          * \param root_parameters_in A description of the root parameters that this resource binder will need to update. Includes
@@ -67,6 +74,7 @@ namespace nova::renderer::rhi {
          * \param descriptor_table_bindings_in All the descriptors in descriptor tables managed by this resource binder
          */
         D3D12ResourceBinder(rx::memory::allocator& allocator,
+                            INT descriptor_size_in,
                             Microsoft::WRL::ComPtr<ID3D12Device> device_in,
                             Microsoft::WRL::ComPtr<ID3D12RootSignature> root_signature_in,
                             rx::vector<D3D12RootParameter> root_parameters_in,
@@ -95,9 +103,11 @@ namespace nova::renderer::rhi {
         void bind_root_descriptor(uint32_t slot_idx, const D3D12RootDescriptor& root_descriptor, ID3D12GraphicsCommandList* cmds) const;
 
     private:
-        rx::map<rx::string, D3D12Image*> bound_images;
-        rx::map<rx::string, D3D12Buffer*> bound_buffers;
-        rx::map<rx::string, D3D12Sampler*> bound_samplers;
+        rx::memory::allocator* internal_allocator;
+
+        rx::map<rx::string, rx::vector<D3D12Image*>> bound_images;
+        rx::map<rx::string, rx::vector<D3D12Buffer*>> bound_buffers;
+        rx::map<rx::string, rx::vector<D3D12Sampler*>> bound_samplers;
 
         Microsoft::WRL::ComPtr<ID3D12Device> device;
         Microsoft::WRL::ComPtr<ID3D12RootSignature> root_signature;
@@ -109,6 +119,18 @@ namespace nova::renderer::rhi {
          */
         rx::map<rx::string, D3D12Descriptor> descriptor_table_bindings;
 
+        INT descriptor_size;
+
         bool is_dirty = false;
+
+        void create_srv_array(const rx::vector<D3D12Image*>& images, const D3D12Descriptor& start_descriptor) const;
+
+        void create_uav_array(const rx::vector<D3D12Image*>& images, const D3D12Descriptor& start_descriptor) const;
+
+        void create_cbv_array(const rx::vector<D3D12Buffer*>& buffers, const D3D12Descriptor& start_descriptor) const;
+
+        void create_srv_array(const rx::vector<D3D12Buffer*>& buffers, const D3D12Descriptor& start_descriptor) const;
+
+        void create_uav_array(const rx::vector<D3D12Buffer*>& buffers, const D3D12Descriptor& start_descriptor) const;
     };
 } // namespace nova::renderer::rhi
