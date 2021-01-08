560299bbce5a266cba41f25fd16e1befdb055f5d
David Dubois <dexcelstraun7@gmail.com>
2021-01-07 22:08:56 -0500

    YEET the submodules

--- external/minitrace.cmake
+++
@@ -1,8 +0,0 @@
-# minitrace has no cmake :(
-    set(MINITRACE_SOURCE ${CMAKE_CURRENT_LIST_DIR}/minitrace/minitrace.c
-    ${CMAKE_CURRENT_LIST_DIR}/minitrace/minitrace.h)
-add_library(minitrace STATIC ${MINITRACE_SOURCE})
-target_include_directories(minitrace SYSTEM PUBLIC
-        $<BUILD_INTERFACE:${CMAKE_CURRENT_LIST_DIR}/minitrace>
-        $<INSTALL_INTERFACE:minitrace>)
-target_compile_definitions(minitrace PUBLIC MTR_ENABLED MTR_COPY_EVENT_CATEGORY_AND_NAME)
